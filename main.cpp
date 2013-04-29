34a0d5c8df2c35dc09dc9a590cec19c2d06a2364
Keith Vetter <keith.vetter@gmail.com>
2013-04-29 11:37:56 -0500

    named it snap

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <QCoreApplication>
-#include <QVector>
 #include <QDebug>
-#include <QtAlgorithms>
-
+#include <QDir>
 #include <stdio.h>
 
 #include "TrickBinaryRiver.hh"
-#include "blame.h"
+#include "snap.h"
 
 void usage();
+bool check_rundir(const QString& rundir);
+bool check_file(const QString& fname);
 
 int main(int argc, char *argv[])
 {
@@ -19,10 +19,15 @@ int main(int argc, char *argv[])
         usage();
     }
 
+    QString rundir(argv[1]);
+
+    if ( ! check_rundir(rundir) ) {
+        usage();
+    }
+
     //Jobs jobs(QString("/home/vetter/dev/blame/RUN_30_iss_wo_hla"));
     //Jobs jobs(QString("/home/vetter/dev/For_Keith"));
     //Jobs jobs(QString("/home/vetter/dev/sims/SIM_threads/RUN_test"));
-    QString rundir(argv[1]);
     Jobs jobs(rundir);
 
     return 0;
@@ -33,4 +38,43 @@ void usage()
     fprintf(stderr,"usage: snap <RUN_directory> (");
     fprintf(stderr,"RUN directory should contain Trick job timing data)\n");
     exit(-1);
+}
+
+bool check_rundir(const QString& rundir)
+{
+    bool ret = true;
+
+    QDir dir(rundir);
+    if ( ! dir.exists() ) {
+        fprintf(stderr,"snap [error] : couldn't find run directory: \"%s\"\n",
+                       rundir.toAscii().constData());
+        return false;
+    }
+
+    QString file_logframe  = rundir + QString("/log_frame.trk");
+    if ( ! check_file(file_logframe) ) {
+        return false;
+    }
+    QString file_trickjobs = rundir + QString("/log_trickjobs.trk");
+    if ( ! check_file(file_trickjobs) ) {
+        return false;
+    }
+    QString file_userjobs  = rundir + QString("/log_userjobs.trk");
+    if ( ! check_file(file_userjobs) ) {
+        return false;
+    }
+
+    return ret;
+}
+
+bool check_file(const QString& fname)
+{
+    QFile file(fname);
+    if ( ! file.exists() || !(file.permissions()&QFile::ReadUser) ) {
+        fprintf(stderr,"snap [error] : can't read or find file %s\n",
+                         fname.toAscii().constData());
+        return false;
+    }
+
+    return true;
 }
