fe78eac02b8dd1cf684d08d9de04afa6933f1914
Keith Vetter <keith.vetter@gmail.com>
2013-08-16 12:30:13 -0500

    added a start/stop convenience method in timit

--- timeit.h
+++ timeit.h
@@ -1,10 +1,18 @@
 #ifndef TIMEIT_H
 #define TIMEIT_H
 
+#include <QDebug>
+#include <QString>
+
 class TimeIt
 {
 public:
     TimeIt();
+    void snap(const char* msg)
+    {
+        qDebug() << msg << stop()/1000000.0;
+        start();
+    }
     virtual void start() = 0;
     virtual long stop() = 0;   // return microseconds since start
 };
