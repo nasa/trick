6e283fbd617a598dad4e823243c87b278b32ba37
Keith Vetter <keith.vetter@gmail.com>
2013-08-16 07:43:43 -0500

    add plot interaction and subclass QCustomPlot

--- snapwindow.cpp
+++ snapwindow.cpp
@@ -12,7 +12,6 @@
 #include <QGridLayout>
 #include <QSplitter>
 
-#include "trickdatamodel.h"
 
 SnapWindow::SnapWindow(Snap *snap, QWidget *parent) :
     QMainWindow(parent),_snap(snap)
@@ -49,7 +48,8 @@ SnapWindow::SnapWindow(Snap *snap, QWidget *parent) :
         tab->addTab(tv,title);
     }
 
-    QSizePolicy sizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+    QSizePolicy sizePolicy(QSizePolicy::MinimumExpanding,
+                           QSizePolicy::MinimumExpanding);
     QFrame* f2 = new QFrame;
     f2->setSizePolicy(sizePolicy);
     s->addWidget(f2);
@@ -59,76 +59,15 @@ SnapWindow::SnapWindow(Snap *snap, QWidget *parent) :
     lay2->setContentsMargins(0, 0, 0, 0);
     lay2->setObjectName(QString::fromUtf8("layout2"));
 
-    QCustomPlot* plot = new QCustomPlot(f2);
-    lay2->addWidget(plot,0,0,1,1);
-
-    plot->setSizePolicy(sizePolicy);
-    plot->setNoAntialiasingOnDrag(true);
-
-    plot->plotLayout()->insertRow(0);
-    plot->plotLayout()->addElement(0, 0, new QCPPlotTitle(plot, ""));
-
-    plot->xAxis->setLabel("Time (s)");
-    plot->yAxis->setLabel("Frame Scheduled Time (s)");
-    plot->legend->setVisible(true);
-    QFont legendFont = font();
-    legendFont.setPointSize(10);
-    plot->legend->setFont(legendFont);
-    plot->legend->setSelectedFont(legendFont);
-    plot->legend->setSelectableParts(QCPLegend::spItems);
-
-    QCPCurve *curve = new QCPCurve(plot->xAxis, plot->yAxis);
-    plot->addPlottable(curve);
-
-    curve->setAntialiased(false);
-
-    QPen graphPen;
-    graphPen.setColor(QColor(0,0,255));
-    graphPen.setWidthF(0);
-    curve->setPen(graphPen);
-
-    //
-    // Set curve data
-    //
     TrickDataModel trick;
     trick.load_binary_trk("log_frame",
                           "/home/vetter/dev/RUN_preCapture_rm2000");
-    for ( int ii = 0; ii < trick.rowCount(); ++ii) {
-        QModelIndex idx = trick.index(ii,0);
-    }
-
-
-    int n = trick.rowCount(); // number of points in graph
-    QModelIndex idx = trick.index(0,0);
-    double _xaxis_min =  trick.data(idx).toDouble();
-    idx = trick.index(n-1,0);
-    double _xaxis_max =  trick.data(idx).toDouble();
-
-    int column = 1;
-    double _yaxis_min = 1.0e20;
-    double _yaxis_max = -1.0e20;
-    QVector<double> x(n), y(n);
-    for ( int i = 0; i < n; ++i )
-    {
-        QModelIndex ix = trick.index(i,0);
-
-        QModelIndex iy = trick.index(i,column);
-        x[i] = trick.data(ix).toDouble();
-        y[i] = trick.data(iy).toDouble()/1000000.0;
-        _yaxis_min = qMin(_yaxis_min,y[i]);
-        _yaxis_max = qMax(_yaxis_max,y[i]);
-    }
-    curve->setData(x, y);
-
-    //
-    // Set curve name
-    //
-    QString param = trick.headerData(column,Qt::Horizontal,
-                                  TrickDataModel::ParamName).toString();
-    curve->setName(param);
-
-    _zoom_to_fit(plot,_xaxis_min,_xaxis_max,
-                      _yaxis_min, _yaxis_max);
+    SnapPlot* plot = new SnapPlot(f2);
+    lay2->addWidget(plot,0,0,1,1);
+    plot->addCurve(trick,0,1);
+    plot->xAxis->setLabel("Time (s)");
+    plot->yAxis->setLabel("Frame Scheduled Time (s)");
+    plot->zoomToFit();
 
     //
     // Resize main window
@@ -150,51 +89,6 @@ SnapWindow::SnapWindow(Snap *snap, QWidget *parent) :
     w += margins;
     frame->setMaximumWidth(w+8);
 }
-    /*
-          //ui->customPlot->setInteractions(QCP::iRangeZoom |
-                                   //QCP::iRangeDrag | QCP::iSelectAxes |
-                                    //QCP::iSelectLegend | QCP::iSelectPlottables);
-
-
-    connect(ui->customPlot, SIGNAL(selectionChangedByUser()),
-            this, SLOT(selectionChanged()));
-    connect(ui->customPlot, SIGNAL(mousePress(QMouseEvent*)),
-            this, SLOT(mousePress(QMouseEvent*)));
-    connect(ui->customPlot, SIGNAL(mouseMove(QMouseEvent*)),
-            this, SLOT(mouseMove(QMouseEvent*)));
-    connect(ui->customPlot, SIGNAL(mouseRelease(QMouseEvent*)),
-            this, SLOT(mouseRelease(QMouseEvent*)));
-    connect(ui->customPlot, SIGNAL(mouseWheel(QWheelEvent*)),
-            this, SLOT(mouseWheel()));
-
-    // make bottom and left axes transfer their ranges to top and right axes:
-    connect(ui->customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)),
-            ui->customPlot->xAxis2, SLOT(setRange(QCPRange)));
-  connect(ui->customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)),
-          ui->customPlot->yAxis2, SLOT(setRange(QCPRange)));
-
-  // connect some interaction slots:
-  connect(ui->customPlot, SIGNAL(titleDoubleClick(QMouseEvent*,QCPPlotTitle*)),
-          this, SLOT(titleDoubleClick(QMouseEvent*,QCPPlotTitle*)));
-  connect(ui->customPlot,
-          SIGNAL(axisDoubleClick(QCPAxis*,QCPAxis::SelectablePart,QMouseEvent*)),
-          this, SLOT(axisLabelDoubleClick(QCPAxis*,QCPAxis::SelectablePart)));
-  connect(ui->customPlot,
-       SIGNAL(legendDoubleClick(QCPLegend*,QCPAbstractLegendItem*,QMouseEvent*)),
-       this, SLOT(legendDoubleClick(QCPLegend*,QCPAbstractLegendItem*)));
-
-  // connect slot that shows a message in the status bar when a graph is clicked:
-  connect(ui->customPlot,
-          SIGNAL(plottableClick(QCPAbstractPlottable*,QMouseEvent*)),
-          this, SLOT(graphClicked(QCPAbstractPlottable*)));
-
-    // This is for popup menu on graph with right click (Keith took this out)
-    ui->customPlot->setContextMenuPolicy(Qt::CustomContextMenu);
-    connect(ui->customPlot, SIGNAL(customContextMenuRequested(QPoint)),
-            this, SLOT(contextMenuRequest(QPoint)));
-            */
-
-
 
 void SnapWindow::createMenu()
 {
@@ -227,11 +121,99 @@ QTableView* SnapWindow::_create_table_view(QAbstractItemModel *model,
 
     return tv;
 }
-
-void SnapWindow::_zoom_to_fit(QCustomPlot* plot,
-                              double xmin, double xmax,
-                              double ymin, double ymax)
+SnapCurve::SnapCurve( QCPAxis* xaxis, QCPAxis* yaxis,
+                     const TrickDataModel &model,
+                     int xcol, int ycol, const QColor &color) :
+    QCPCurve(xaxis,yaxis)
 {
+    QPen graphPen;
+    graphPen.setColor(color);
+    graphPen.setWidthF(0);
+    setPen(graphPen);
+    setAntialiased(false);
+
+    //
+    // Set curve data
+    //
+    int n = model.rowCount(); // number of points in graph
+    QModelIndex idx = model.index(0,0);
+    _xmin =  model.data(idx).toDouble();
+    idx = model.index(n-1,0);
+    _xmax =  model.data(idx).toDouble();
+
+    _ymin = 1.0e20;
+    _ymax = -1.0e20;
+    QVector<double> x(n), y(n);
+    for ( int i = 0; i < n; ++i )
+    {
+        QModelIndex ix = model.index(i,xcol);
+
+        QModelIndex iy = model.index(i,ycol);
+
+        x[i] = model.data(ix).toDouble();
+        y[i] = model.data(iy).toDouble()/1000000.0;
+        _ymin = qMin(_ymin,y[i]);
+        _ymax = qMax(_ymax,y[i]);
+    }
+    setData(x, y);
+
+    //
+    // Set curve name
+    //
+    QString param = model.headerData(ycol,Qt::Horizontal,
+                                  TrickDataModel::ParamName).toString();
+    setName(param);
+}
+
+SnapPlot::SnapPlot(QWidget* parent) :
+    QCustomPlot(parent),
+    _rubber_band(0)
+{
+    QSizePolicy sizePolicy(QSizePolicy::MinimumExpanding,
+                           QSizePolicy::MinimumExpanding);
+    setSizePolicy(sizePolicy);
+
+    setNoAntialiasingOnDrag(true);
+
+    // Title (blank for now)
+    plotLayout()->insertRow(0);
+    plotLayout()->addElement(0, 0, new QCPPlotTitle(this, ""));
+
+    QFont legendFont = font();
+    legendFont.setPointSize(10);
+    legend->setFont(legendFont);
+    legend->setSelectedFont(legendFont); legend->setSelectableParts(QCPLegend::spItems); legend->setVisible(true);
+
+    setInteractions(QCP::iRangeZoom |
+                    QCP::iRangeDrag | QCP::iSelectAxes |
+                    QCP::iSelectLegend | QCP::iSelectPlottables);
+}
+
+SnapCurve* SnapPlot::addCurve(const TrickDataModel &model,
+                              int xcol, int ycol,
+                              const QColor& color)
+{
+    SnapCurve *curve = new SnapCurve(xAxis, yAxis, model, xcol, ycol, color);
+    _curves.append(curve);
+    addPlottable(curve);
+
+    //_zoom_to_fit(plot,_xaxis_min,_xaxis_max,_yaxis_min,_yaxis_max);
+    return curve;
+}
+
+void SnapPlot::zoomToFit()
+{
+    double xmin = 1.0e20;
+    double ymin = 1.0e20;
+    double xmax = -1.0e20;
+    double ymax = -1.0e20;
+    foreach ( SnapCurve* curve, _curves ) {
+        if ( curve->xmin() < xmin ) xmin = curve->xmin();
+        if ( curve->ymin() < ymin ) ymin = curve->ymin();
+        if ( curve->xmax() > xmax ) xmax = curve->xmax();
+        if ( curve->ymax() > ymax ) ymax = curve->ymax();
+    }
+
     // At least show +/- 10% of outside range
     double xp = 0.10*qAbs(xmax - xmin) ;
     double yp = 0.10*qAbs(ymax - ymin) ;
@@ -249,10 +231,67 @@ void SnapWindow::_zoom_to_fit(QCustomPlot* plot,
             break;
         }
     }
-    plot->xAxis->setRange(xmin,xmax+xp);
-    plot->yAxis->setRange(ymin,ymax+yp);
-    plot->axisRect()->setupFullAxesBox();
-    plot->replot();
+    xAxis->setRange(xmin,xmax+xp);
+    yAxis->setRange(ymin,ymax+yp);
+    axisRect()->setupFullAxesBox();
+    replot();
+}
+
+
+
+void SnapPlot::mousePressEvent(QMouseEvent *event)
+{
+    if ( event->button() == Qt::MidButton ){
+        _origin = event->pos();
+        if ( !_rubber_band ) {
+            _rubber_band = new QRubberBand(QRubberBand::Rectangle,this);
+        }
+        _rubber_band->setGeometry(QRect(_origin, QSize()));
+        _rubber_band->show();
+    } else  if ( event->button() == Qt::RightButton ){
+        zoomToFit();
+    } else {
+        // if an axis is selected, only drag in direction of that axis
+        // if no axis is selected, both directions may be dragged
+        if (xAxis->selectedParts().testFlag(QCPAxis::spAxis)) {
+            axisRect()->setRangeDrag(xAxis->orientation());
+        } else if (yAxis->selectedParts().testFlag(QCPAxis::spAxis)) {
+            axisRect()->setRangeDrag(yAxis->orientation());
+        } else {
+            axisRect()->setRangeDrag(Qt::Horizontal|Qt::Vertical);
+        }
+    }
+
+    QCustomPlot::mousePressEvent(event);
+}
+
+void SnapPlot::mouseMoveEvent(QMouseEvent *event)
+{
+    if ( event->button() == Qt::NoButton && _rubber_band ){
+        _rubber_band->setGeometry(QRect(_origin, event->pos()).normalized());
+    }
+    QCustomPlot::mouseMoveEvent(event);
+}
+
+void SnapPlot::mouseReleaseEvent(QMouseEvent *event)
+{
+    if ( event->button() == Qt::MidButton && _rubber_band ) {
+        QRect geom = _rubber_band->geometry();
+        double gxmin = (double)geom.left();
+        double gxmax = (double)geom.right();
+        double xmin = xAxis->pixelToCoord(gxmin);
+        double xmax = xAxis->pixelToCoord(gxmax);
+        double gymin = (double)geom.top();
+        double gymax = (double)geom.bottom();
+        double ymin = yAxis->pixelToCoord(gymax); // screen flip
+        double ymax = yAxis->pixelToCoord(gymin); // screen flip
+        xAxis->setRange(xmin,xmax);
+        yAxis->setRange(ymin,ymax);
+        replot();
+        _rubber_band->hide();
+    }
+
+    QCustomPlot::mouseReleaseEvent(event);
 }
 
 #endif // SNAPGUI
--- snapwindow.h
+++ snapwindow.h
@@ -9,9 +9,61 @@
 #include <QGridLayout>
 #include <QTableView>
 #include <QAction>
+#include <QRubberBand>
 
 #include "snap.h"
 #include "qplot/qcustomplot.h"
+#include "trickdatamodel.h"
+
+class SnapCurve : public QCPCurve
+{
+  public:
+    SnapCurve(QCPAxis* xaxis, QCPAxis* yaxis,
+              const TrickDataModel &model, int xcol, int ycol,
+              const QColor& color=Qt::blue);
+
+    double xmin() { return _xmin; }
+    double xmax() { return _xmax; }
+    double ymin() { return _ymin; }
+    double ymax() { return _ymax; }
+
+  private:
+    double _xmin;
+    double _ymin;
+    double _xmax;
+    double _ymax;
+
+};
+
+class SnapPlot : public QCustomPlot
+{
+  public:
+    SnapPlot(QWidget* parent=0);
+
+   //SnapCurve*    curve(int index) const;
+    SnapCurve* addCurve(const TrickDataModel &model,
+                       int xcol, int ycol,
+                       const QColor& color=Qt::blue);
+    //bool removeCurve(int index);
+    //int clearCurves();
+    //int curveCount() const;
+
+    void zoomToFit();
+
+protected:
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseMoveEvent(QMouseEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
+
+  private:
+    QList<SnapCurve*> _curves;
+    void _set_interactions();
+
+    QPoint _origin;
+    QRubberBand* _rubber_band;
+
+
+};
 
 class SnapWindow : public QMainWindow
 {
@@ -32,8 +84,8 @@ private:
     QList<QTableView*> _tvs;
     QTableView *_create_table_view(QAbstractItemModel* model,
                                    Qt::Orientation orientation);
-    void _zoom_to_fit(QCustomPlot* plot,
-                      double xmin, double xmax, double ymin, double ymax);
+
+    void _plot_set_interactions(QCustomPlot* plot);
 
 signals:
     
