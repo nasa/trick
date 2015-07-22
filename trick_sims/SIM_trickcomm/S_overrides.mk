b3d1a1d1629bab16126a3608d3f262f71a260a54
Alex Lin <alexander.s.lin@nasa.gov>
2015-07-22 15:34:54 -0500

    Trick should not modify TRICK_CFLAGS and TRICK_CXXFLAGS
    Well sims compiled, but I forgot to test if Trick itself
    would compile.  It didn't.  Added TRICK_SYSTEM_CFLAGS and
    TRICK_SYSTEM_CXXFLAGS where needed.
    
    refs #90

--- trick_sims/SIM_trickcomm/S_overrides.mk
+++ trick_sims/SIM_trickcomm/S_overrides.mk
@@ -4,10 +4,10 @@ TRICK_CXXFLAGS += -I${TRICK_HOME}/trick_models
 
 S_main: tc_server tc_client
 tc_server: tc_server.c
-	${TRICK_CC} ${TRICK_CFLAGS} -I${TRICK_HOME}/trick_source -o $@ $< -L${TRICK_LIB_DIR} -ltrick_comm ${TRICK_EXEC_LINK_LIBS}
+	${TRICK_CC} ${TRICK_CFLAGS} ${TRICK_SYSTEM_CFLAGS} -I${TRICK_HOME}/trick_source -o $@ $< -L${TRICK_LIB_DIR} -ltrick_comm ${TRICK_EXEC_LINK_LIBS}
 
 tc_client: tc_client.c
-	${TRICK_CC} ${TRICK_CFLAGS} -I${TRICK_HOME}/trick_source -o $@ $< -L${TRICK_LIB_DIR} -ltrick_comm ${TRICK_EXEC_LINK_LIBS}
+	${TRICK_CC} ${TRICK_CFLAGS} ${TRICK_SYSTEM_CFLAGS} -I${TRICK_HOME}/trick_source -o $@ $< -L${TRICK_LIB_DIR} -ltrick_comm ${TRICK_EXEC_LINK_LIBS}
 
 clean: clean_tc_apps
 spotless: clean_tc_apps
