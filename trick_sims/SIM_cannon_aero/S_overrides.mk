6492e2509a1c684568871f8cd766e4963265bcec
John M. Penn <john.m.penn@nasa.gov>
2015-09-04 11:01:48 -0500

    Add check that TRICK_HOME is set in S_overrides.mk.

--- trick_sims/SIM_cannon_aero/S_overrides.mk
+++ trick_sims/SIM_cannon_aero/S_overrides.mk
@@ -1,2 +1,5 @@
+ifndef TRICK_HOME
+    $(error TRICK_HOME environment variable is not set. Please set it and retry.)
+endif
 TRICK_CFLAGS += -I${TRICK_HOME}/trick_models
 TRICK_CXXFLAGS += -I${TRICK_HOME}/trick_models
