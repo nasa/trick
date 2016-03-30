84f1fd0b5c908ff357dc8ad0838b66be9050fbd6
John M. Penn <john.m.penn@nasa.gov>
2016-03-30 17:25:08 -0500

    Created a graphical variable server client for SIM_sun. Refs #208

--- trick_sims/SIM_sun/RUN_test/input.py
+++ trick_sims/SIM_sun/RUN_test/input.py
@@ -1,3 +1,4 @@
+
 trick_sys.sched.set_enable_freeze(True)
 
 """ Sim Control Panel """
@@ -26,9 +27,13 @@ if STRIPCHART:
 """ ======================================== """
 """                 LOCATION                 """
 """ ======================================== """
-""" JSC MAIN Gate """
+# JSC MAIN Gate
 sun_predictor.sun.observer_latitude  = trick.sim_services.attach_units("d" , 29.55298)
 sun_predictor.sun.observer_longitude = trick.sim_services.attach_units("d" , 95.09379)
+
+# Mom's House
+#sun_predictor.sun.observer_latitude  = trick.sim_services.attach_units("d" , 38.842677)
+#sun_predictor.sun.observer_longitude = trick.sim_services.attach_units("d" , 83.19773)
 
 
 """ ======================================== """
@@ -37,13 +42,20 @@ sun_predictor.sun.observer_longitude = trick.sim_services.attach_units("d" , 95.
 """ appropriate line below.                  """
 """ ======================================== """
 #sun_predictor.sun.observer_offset_from_UTC = -4
+
 """ Eastern Standard Time """
 #sun_predictor.sun.observer_offset_from_UTC = -5
+
+
+""" Eastern Daylight Time """
+#sun_predictor.sun.observer_offset_from_UTC = -4
+
+
 """ Central Daylight Time """
-#sun_predictor.sun.observer_offset_from_UTC = -5
+sun_predictor.sun.observer_offset_from_UTC = -5
 
 """ Central Standard Time """
-sun_predictor.sun.observer_offset_from_UTC = -6
+#sun_predictor.sun.observer_offset_from_UTC = -6
 
 """ Mountain Daylight Time """
 #sun_predictor.sun.observer_offset_from_UTC = -6
@@ -59,12 +71,38 @@ sun_predictor.sun.observer_offset_from_UTC = -6
 """               LOCAL TIME                 """
 """ Set local time here.                     """
 """ ======================================== """
-sun_predictor.sun.local_time.year  = 2016
-sun_predictor.sun.local_time.month = 3
-sun_predictor.sun.local_time.day   = 11
-sun_predictor.sun.local_time.hour  = 17
-sun_predictor.sun.local_time.min   = 10
-sun_predictor.sun.local_time.sec   = 0.0
+#sun_predictor.sun.local_time.year  = 2016
+#sun_predictor.sun.local_time.month = 3
+#sun_predictor.sun.local_time.day   = 14
+#sun_predictor.sun.local_time.hour  = 14
+#sun_predictor.sun.local_time.min   = 33
+#sun_predictor.sun.local_time.sec   = 0.0
+
+import datetime
+current_local_time =  datetime.datetime.now()
+sun_predictor.sun.local_time.year   = current_local_time.year
+sun_predictor.sun.local_time.month  = current_local_time.month
+sun_predictor.sun.local_time.day    = current_local_time.day
+sun_predictor.sun.local_time.hour   = current_local_time.hour
+sun_predictor.sun.local_time.min    = current_local_time.minute
+sun_predictor.sun.local_time.sec    = current_local_time.second
+
+#==========================================
+# Start the display VarServer Client
+#==========================================
+varServerPort = trick.var_server_get_port();
+SunDisplay_path = "models/Graphics/dist/SunDisplay.jar"
+
+if (os.path.isfile(SunDisplay_path)) :
+    SunDisplay_cmd = "java -jar " \
+                   + SunDisplay_path \
+                   + " " + str(varServerPort) + " &" ;
+    print(SunDisplay_cmd)
+    os.system( SunDisplay_cmd);
+else :
+    print('==================================================================================')
+    print('SunDisplay needs to be built. Please \"cd\" into models/Graphics and type \"make\".')
+    print('==================================================================================')
 
 trick.stop(86400.0)
 
