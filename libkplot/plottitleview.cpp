9ac24a7e1e058fe51e9d6cab93fb776904e4f9a4
vetter <keith.vetter@gmail.com>
2016-05-10 10:37:14 -0500

    simplify PlotTitleView by inheriting from BookIdxView

--- libkplot/plottitleview.cpp
+++ libkplot/plottitleview.cpp
@@ -1,8 +1,7 @@
 #include "plottitleview.h"
 
 PlotTitleView::PlotTitleView(QWidget *parent) :
-    QAbstractItemView(parent),
-    _plotTitleIdx(QModelIndex())
+    BookIdxView(parent)
 {
     setFrameShape(QFrame::NoFrame);
 
@@ -16,176 +15,50 @@ PlotTitleView::PlotTitleView(QWidget *parent) :
     setLayout(_vBoxLayout);
 }
 
-void PlotTitleView::setModel(QAbstractItemModel *model)
+void PlotTitleView::_update()
 {
-    if ( model == this->model() ) {
-        QAbstractItemView::setModel(model);
+    PlotBookModel* bookModel = _bookModel();
+
+    if ( !bookModel->isChildIndex(_myIdx,"Plot","PlotTitle") ) {
         return;
     }
 
-    // Allow setting model to null
-    if ( !model ) {
-        _plotTitleIdx = QModelIndex();
-        QAbstractItemView::setModel(model);
-        return;
-    }
+    QModelIndex plotTitleIdx  = bookModel->getIndex(_myIdx, "PlotTitle","Plot");
+    plotTitleIdx = bookModel->index(plotTitleIdx.row(),1,_myIdx);
+    QString plotTitle = bookModel->data(plotTitleIdx).toString();
 
-    PlotBookModel* bookModel = dynamic_cast<PlotBookModel*>(model);
-    if ( !bookModel ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::setModel() "
-                    "could not cast model at address"
-                 << (void*)model
-                 << "to a PlotBookModel";
-        exit(-1);
-    }
-
-    if ( _plotTitleIdx.isValid() ) {
-        if ( _plotTitleIdx.model() != model ) {
-            qDebug() << "snap [bad scoobs]: PlotTitleView::setModel() "
-                        "is being set to a model that is not the same "
-                        "as _plotTitleIdx.model()"
-                     << (void*)model
-                     << "to a PlotBookModel";
-            exit(-1);
-        }
-    }
-
-    QAbstractItemView::setModel(model);
+    _label->setText(plotTitle);
 }
 
-void PlotTitleView::setRootIndex(const QModelIndex &plotIdx)
-{
-    if( !plotIdx.isValid() ) {
-        QAbstractItemView::setRootIndex(plotIdx);
-        return;
-    }
-
-    if ( plotIdx.model() == 0 ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::setRootIndex() "
-                 << "to a plotIdx with null model";
-        exit(-1);
-    }
-
-    if ( !model() ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::setRootIndex() "
-                    "called without setting the view model. "
-                    "You must call view->setModel() before setting the "
-                    "page index";
-        exit(-1);
-    }
-
-    if ( plotIdx.model() != this->model() ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::setRootIndex() "
-                    "set to index with model which does not match "
-                    "view->model().  Please setModel() to model that "
-                    "is same as plotIdx's model.";
-        exit(-1);
-    }
-
-    PlotBookModel* bookModel = dynamic_cast<PlotBookModel*>(model());
-    if ( !bookModel ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::setRootIndex() "
-                    "could not cast model at address"
-                 << (void*)model()
-                 << "to a PlotBookModel";
-        exit(-1);
-    }
-
-    if ( model()->data(plotIdx).toString() != "Plot" ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::setRootIndex() "
-                    "given plotIdx, but parent is not a PlotIdx.";
-        exit(-1);
-    }
-
-    _plotIdx = plotIdx;
-
-    if ( bookModel->isChildIndex(plotIdx,"Plot","PlotTitle") ) {
-        _plotTitleIdx = bookModel->getIndex(plotIdx,"PageTitle","Page");
-        _plotTitleIdx = model()->index(_plotTitleIdx.row(),1,plotIdx);
-        QString title = model()->data(_plotTitleIdx).toString();
-        _label->setText(title);
-    }
-
-}
 
 // TODO: For now and only handle single item changes
 void PlotTitleView::dataChanged(const QModelIndex &topLeft,
                                 const QModelIndex &bottomRight)
 {
-    if ( topLeft != _plotTitleIdx ) return;
+    // TODO: For now and only handle single item changes
+    if ( topLeft != bottomRight ) return;
 
-    QString title = model()->data(topLeft).toString();
-    _label->setText(title);
+    // Value is in column 1
+    if ( topLeft.column() != 1 ) return;
+
+    QModelIndex pidx = topLeft.parent();
+    if ( pidx != _myIdx ) return;
+
+    _update();
 }
 
 // TODO: only single append works
 void PlotTitleView::rowsInserted(const QModelIndex &pidx, int start, int end)
 {
-    if ( pidx != _plotIdx ) return;
+    if ( pidx != _myIdx ) return;
 
-    QModelIndex cidx = model()->index(start,0,pidx);
-    if ( model()->data(cidx).toString() != "PlotTitle" ) return;
-
-    _plotTitleIdx = model()->index(start,1,pidx);
-
-    PlotBookModel* bookModel = dynamic_cast<PlotBookModel*>(model());
-    if ( !bookModel ) {
-        qDebug() << "snap [bad scoobs]: PlotTitleView::rowsInserted() "
-                    "could not cast model at address"
-                 << (void*)model()
-                 << "to a PlotBookModel";
-        exit(-1);
+    for ( int i = start; i <= end; ++i ) {
+        QModelIndex idx = model()->index(i,0,pidx);
+        QString cText = model()->data(idx).toString();
+        if ( cText == "PlotTitle" ) {
+            _update();
+        }
     }
 
-    QString title = model()->data(_plotTitleIdx).toString();
-    _label->setText(title);
-
     return;
-}
-
-QModelIndex PlotTitleView::indexAt(const QPoint &point) const
-{
-    QModelIndex idx;
-    return idx;
-}
-
-QRect PlotTitleView::visualRect(const QModelIndex &index) const
-{
-}
-
-void PlotTitleView::scrollTo(const QModelIndex &index,
-                             QAbstractItemView::ScrollHint hint)
-{
-}
-
-QModelIndex PlotTitleView::moveCursor(
-        QAbstractItemView::CursorAction cursorAction,
-        Qt::KeyboardModifiers modifiers)
-{
-}
-
-int PlotTitleView::horizontalOffset() const
-{
-    return 0;
-}
-
-int PlotTitleView::verticalOffset() const
-{
-    return 0;
-}
-
-// TODO: the index can be hidden
-bool PlotTitleView::isIndexHidden(const QModelIndex &index) const
-{
-    return false;
-}
-
-void PlotTitleView::setSelection(const QRect &rect,
-                                 QItemSelectionModel::SelectionFlags command)
-{
-}
-
-QRegion PlotTitleView::visualRegionForSelection(
-        const QItemSelection &selection) const
-{
 }
--- libkplot/plottitleview.h
+++ libkplot/plottitleview.h
@@ -1,52 +1,27 @@
 #ifndef PLOTTITLEVIEW_H
 #define PLOTTITLEVIEW_H
 
-#include <QAbstractItemView>
 #include <QLabel>
 #include <QVBoxLayout>
-#include "libqplot/plotbookmodel.h"
+#include "bookidxview.h"
 
-class PlotTitleView : public QAbstractItemView
+class PlotTitleView : public BookIdxView
 {
     Q_OBJECT
 public:
     explicit PlotTitleView(QWidget *parent = 0);
 
-    virtual void setModel(QAbstractItemModel *model);
-    virtual QModelIndex indexAt( const QPoint& point) const;
-    virtual QRect visualRect(const QModelIndex &index) const;
-    virtual void scrollTo(const QModelIndex &index,
-                          ScrollHint hint = EnsureVisible);
-
 protected:
-    virtual QModelIndex moveCursor(CursorAction cursorAction,
-                                   Qt::KeyboardModifiers modifiers);
-    virtual int horizontalOffset() const;
-    virtual int verticalOffset() const;
-    virtual bool isIndexHidden(const QModelIndex &index) const;
-    virtual void setSelection(const QRect &rect,
-                              QItemSelectionModel::SelectionFlags command);
-    virtual QRegion visualRegionForSelection(
-                              const QItemSelection &selection) const;
+    virtual void _update();
 
 private:
-    QPersistentModelIndex _plotIdx;
-    QPersistentModelIndex _plotTitleIdx;
     QLabel* _label;
     QVBoxLayout* _vBoxLayout;
-
-signals:
-
-public slots:
-    virtual void setRootIndex(const QModelIndex &plotIdx);
 
 protected slots:
     virtual void dataChanged(const QModelIndex &topLeft,
                              const QModelIndex &bottomRight);
     virtual void rowsInserted(const QModelIndex &parent, int start, int end);
-    //virtual void rowsAboutToBeRemoved(const QModelIndex &parent, int start, int end);
-    //virtual void selectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
-    //virtual void currentChanged(const QModelIndex &current, const QModelIndex &previous);
 
 };
 
