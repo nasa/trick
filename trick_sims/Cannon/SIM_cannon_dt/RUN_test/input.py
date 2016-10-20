4f9c1cdc0be833ee8db33b6dac842192c607f2ea
John M. Penn <john.m.penn@nasa.gov>
2016-10-20 17:35:27 -0500

    Get rid of SIM_cannon_dt and combine Sim_cannon_contact and SIM_cannon_integ

--- trick_sims/Cannon/SIM_cannon_dt/RUN_test/input.py
+++
@@ -1,5 +0,0 @@
-
-execfile("Modified_data/realtime.py")
-execfile("Modified_data/cannon.dr")
-
-trick.exec_set_terminate_time(5.2)
--- trick_sims/Cannon/SIM_cannon_integ/RUN_test/input.py
+++
@@ -1,9 +0,0 @@
-
-#execfile("Modified_data/realtime.py")
-execfile("Modified_data/cannon.dr")
-
-trick.TMM_reduced_checkpoint(0)
-
-dyn_integloop.getIntegrator(trick.Runge_Kutta_4, 4)
-
-trick.stop(6)
