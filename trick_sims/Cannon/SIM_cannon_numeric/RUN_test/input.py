37a75bc34e1292c6fdd9601ba35df2187e3d96e8
John M. Penn <john.m.penn@nasa.gov>
2016-10-20 19:12:31 -0500

    Update SIM_cannon_numeric

--- trick_sims/Cannon/SIM_cannon_numeric/RUN_test/input.py
+++ trick_sims/Cannon/SIM_cannon_numeric/RUN_test/input.py
@@ -1,6 +1,5 @@
 
-#execfile("Modified_data/realtime.py")
+execfile("Modified_data/realtime.py")
 execfile("Modified_data/cannon.dr")
 
-dyn_integloop.getIntegrator(trick.Runge_Kutta_4, 4)
-trick.exec_set_terminate_time(5.2)
+dyn_integloop.getIntegrator(trick.Runge_Kutta_4, 5)
