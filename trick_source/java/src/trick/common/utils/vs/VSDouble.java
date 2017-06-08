fd3f7e14406cf7b3c894828263de62f5c2be5266
Christopher LaChance <christopher.t.lachance@gmail.com>
2017-06-08 10:39:13 -0500

    Lifted fix logic into a protected function in the super class.
    Removed author/date information.
    Put duplicate code from VSFLoat and VSDouble into VSValue.
    Added support for -NaN.

--- trick_source/java/src/trick/common/utils/vs/VSDouble.java
+++ trick_source/java/src/trick/common/utils/vs/VSDouble.java
@@ -12,24 +12,7 @@ public class VSDouble extends VSValue<Double> {
 
     @Override
     public void fromVariableServer(String string) {
-        // CTLaChance
-        // JUN 07 2017
-        /*
-        Converts the C representation of +- infinity and +- not-a-number to the Java representation
-        so that Double.parseDouble() can correctly assign those values to the variable value.
-        -NaN does not exist in Java and is therefore converted to NaN.
-        */
-        if(string.equals("inf")) {
-            string = "Infinity";
-        }
-        else if(string.equals("-inf")) {
-            string = "-Infinity";
-        }
-        else if(string.equals("-nan") || string.equals("nan")) {
-            string = "NaN";
-        }
-
-        setValue(Double.parseDouble(string.trim()));
+        setValue(Double.parseDouble(handleUndefinedValues(string).trim()));
     }
 
     @Override
