16595f3f3d84af9eca33f2188020a71e1165da92
Scott Fennell <spfennell@gmail.com>
2018-04-21 10:49:20 -0500

    #604 #490 add java.se.ee to application at runtime.

--- libexec/trick/pm/launch_java.pm
+++ libexec/trick/pm/launch_java.pm
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 package launch_java ;
-@ISA = qw(Exporter);
-@EXPORT = qw(launch_java);
+@ISA = qw(Exporter) ;
+@EXPORT = qw(launch_java) ;
 
 # launch_java determines trick_home based on execution location of the calling script.
 use File::Basename ;
-use Cwd 'abs_path';
+use Cwd 'abs_path' ;
 use gte ;
 
 sub launch_java($$) {
@@ -26,6 +26,10 @@ sub launch_java($$) {
         $lib_dir = "lib" ;
     }
     $java_dir = "$ENV{TRICK_HOME}/$lib_dir/trick/java" ;
+    
+    $javac_version = `javac -version 2>&1` ;
+    $javac_version =~  /(\d+)/ ;
+    $javac_version = $1 ;
 
     $host_cpu = gte("TRICK_HOST_CPU") ;
     chomp($host_cpu) ;
@@ -35,18 +39,23 @@ sub launch_java($$) {
         $command = "java -classpath $java_dir/dist/*:$java_dir/lib/*:$java_dir/lib/ \\
              -Xdock:name=\"$name\" \\
              -Xdock:icon=$java_dir/resources/trick_icon.png \\
-             -Djava.net.preferIPv4Stack=true \\
-             $application" ;
+             -Djava.net.preferIPv4Stack=true \\" ;        
     } else {
-        $command = "java -cp $java_dir/dist/*:$java_dir/lib/*:$java_dir/lib/ $application" ;
+        $command = "java -cp $java_dir/dist/*:$java_dir/lib/*:$java_dir/lib/ \\" ;
     }
+    if ( ($javac_version eq "9") or ($javac_version eq "10") ) {
+        $command .= "
+             --add-modules java.se.ee \\" ;
+    }
+    $command .= "
+             $application" ;
 
     foreach (@ARGV) {
-       $command .= " $_";
+       $command .= " $_" ;
     }
 
     system $command ;
-    exit $? >> 8;
+    exit $? >> 8 ;
 }
 
-1;
+1 ;
