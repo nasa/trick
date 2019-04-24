7ccdade44155bd71cb895e44d4484c7671727a86
Penn, John M 047828115 <john.m.penn@nasa.gov>
2019-04-24 18:50:08 -0500

    Fix S_overrides.mk #756

--- trick_sims/Cannon/SIM_cannon_numeric/S_overrides.mk
+++ trick_sims/Cannon/SIM_cannon_numeric/S_overrides.mk
@@ -2,5 +2,5 @@ TRICK_CFLAGS   += -I../models
 TRICK_CXXFLAGS += -I../models
 TRICK_SFLAGS   += -I../models
 
-TRICK_LDFLAGS += -L/usr/local/lib
-TRICK_USER_LINK_LIBS += -lmongoose
+#TRICK_LDFLAGS += -L/usr/local/lib
+#TRICK_USER_LINK_LIBS += -lmongoose
