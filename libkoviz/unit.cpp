d53447092fc4d06f071f22d95d986e807976f59e
Keith Vetter <keith.vetter@gmail.com>
2019-05-30 12:12:21 -0500

    Add N*rad/m2 unit

--- libkoviz/unit.cpp
+++ libkoviz/unit.cpp
@@ -414,6 +414,7 @@ QHash<QPair<QString, QString>, double> Unit::_initScales()
     map.insert(QPair<QString,QString>("N*s/m2","N*s/m2"), 1.0);
     map.insert(QPair<QString,QString>("s/s","s/s"), 1.0);
     map.insert(QPair<QString,QString>("lbf*degree/ft2","lbf*degree/ft2"), 1.0);
+    map.insert(QPair<QString,QString>("N*rad/m2","N*rad/m2"), 1.0);
 
     return map;
 }
