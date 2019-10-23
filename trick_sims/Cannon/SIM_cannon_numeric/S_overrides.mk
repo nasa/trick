95c66597331ad56959cbe7f5297e228c6022496a
jmpenn <john.m.penn@nasa.gov>
2019-10-23 11:58:19 -0500

    Incorporate Webserver into Trick, so one only has to include HttpServ… (#886)
    * Incorporate Webserver into Trick, so one only has to include HttpServer.sm
    
    * Tweaks in default index.html file
    
    * Rename HTTPServer.sm to WebServer.sm
    
    * Rename http_server to WebServer
    
    * Add --retry to curl invocations in HttpServer makefile.
    
    * Fix #include in VariableServerVariable.hh
    
    * Include cleanup and curl tweaks in the hopes of making Jenkins happy.
    
    * Doh! problem in makefile masked by preinstalled mongoose in usr/local/lib
    
    * DIE Make Bug DIE
    
    * Fix include in WebServer.sm
    
    * WebServer.sm constructor name
    
    * Don't SWIG mongoose.h
    
    * Compile with -std=c++11
    
    * Attempt to fix race condition in makefile
    
    * makefie tweek
    
    * Fix trick library name problem for Centos and Redhat

--- trick_sims/Cannon/SIM_cannon_numeric/S_overrides.mk
+++ trick_sims/Cannon/SIM_cannon_numeric/S_overrides.mk
@@ -1,8 +1,2 @@
 TRICK_CFLAGS   += -I../models
 TRICK_CXXFLAGS += -I../models
-
-# Uncomment these for an embedded webserver.
-#TRICK_SFLAGS += -DTRICK_HTTP
-#TRICK_SFLAGS   += -I../models
-#TRICK_LDFLAGS += -L/usr/local/lib
-#TRICK_USER_LINK_LIBS += -lmongoose
