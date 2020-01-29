1b43a82f532a3546680815efc1514781613f60ef
Scott Fennell <spfennell@gmail.com>
2020-01-29 14:11:46 -0600

    refactor java build to use maven to resolve dependencies and build executable jars #938

--- libexec/trick/pm/launch_java.pm
+++ libexec/trick/pm/launch_java.pm
@@ -31,22 +31,22 @@ sub launch_java($$) {
     chomp($host_cpu) ;
     $ENV{TRICK_HOST_CPU} = $host_cpu ;
 
+    $command = "java -jar ";
+
     if ( $^O eq "darwin" ) {
-        $command = "java -classpath $java_dir/dist/*:$java_dir/lib/*:$java_dir/lib/ \\
+        $command .= "\\
              -Xdock:name=\"$name\" \\
              -Xdock:icon=$java_dir/resources/trick_icon.png \\
              -Djava.net.preferIPv4Stack=true \\" ;        
-    } else {
-        $command = "java -cp $java_dir/dist/*:$java_dir/lib/*:$java_dir/lib/ \\" ;
     }
 
-    $command .= "
-             $application" ;
+    $command .= "$java_dir/build/$application.jar ";
 
     foreach (@ARGV) {
-       $command .= " $_" ;
+       $command .= "$_ ";
     }
 
+    print($command);
     system $command ;
     exit $? >> 8 ;
 }
