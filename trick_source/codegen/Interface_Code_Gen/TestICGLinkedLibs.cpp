3a9ca56c05d61d24fada4171696a7b33d224e872
Fennell, Scott P 263712616 <scott.p.fennell@nasa.gov>
2020-09-13 16:34:44 -0500

    #1053 #1009 cmake configure correct clang libs

---
+++ trick_source/codegen/Interface_Code_Gen/TestICGLinkedLibs.cpp
@@ -0,0 +1,17 @@
+#include "llvm/Support/Host.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "clang/Basic/Builtins.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Lex/PreprocessorOptions.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Parse/ParseAST.h"
+
+int main() {
+	return 0;
+}
