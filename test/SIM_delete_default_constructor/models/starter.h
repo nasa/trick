a2bbbc5dcf3c2b16b43bfdc087c4de014b4b255e
Fennell, Scott P 263712616 <scott.p.fennell@nasa.gov>
2021-02-05 02:40:16 -0600

    unit test icg deleted default constructor #1094

---
+++ test/SIM_delete_default_constructor/models/starter.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+PURPOSE: (Starter class)
+LIBRARY DEPENDENCY:
+    (
+    (starter.cpp)
+    )
+**************************************************************************/
+
+class Starter {
+public:
+    Starter() = delete;
+    Starter(int foo);
+
+};
