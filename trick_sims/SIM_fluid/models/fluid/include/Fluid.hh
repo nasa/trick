84c810e44709056e4ec48fb512746f85c618f35a
Nate Hill <nateh777@cs.utexas.edu>
2021-06-10 10:53:27 -0500

    Store particles as an std::vector instead of an array

--- trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
+++ trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
@@ -4,16 +4,17 @@ PURPOSE: (Simulate a fluid using smoothed particle hydrodynamics (SPH).)
 #ifndef _fluid_hh_
 #define _fluid_hh_
 #include "Particle.hh"
+#include <vector>
 
 
 class Fluid {
 	public:
 		Fluid(){}
-		Particle** particles;
+		std::vector<Particle> particles;
 		unsigned int n_particles;
 		
 		int default_data();
-		int update_SPH_GPU();
+		int update_SPH();
 };
 
 #endif
