9a10cef9d78505b648660b79bae82ba4dad7a514
Nate Hill <nateh777@cs.utexas.edu>
2021-06-11 16:44:34 -0500

    Remove static qualifier from simulation parameters in Fluid.hh

--- trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
+++ trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
@@ -22,38 +22,38 @@ class Fluid {
 		int default_data();
 		int update_SPH();
 		
-		const static float G_STRENGTH = 600;
+		const float G_STRENGTH = 600;
 		// external graviational force
-		const static float G[3] = {0.f, G_STRENGTH * -9.8f, 0.f};
-		const static float REST_DENS = 1000.f;
-		const static float GAS_CONST = 100.f;
+		const float G[3] = {0.f, G_STRENGTH * -9.8f, 0.f};
+		const float REST_DENS = 1000.f;
+		const float GAS_CONST = 100.f;
 		// kernel radius
-		const static float H = 16.f;
-		const static float HSQ = H * H;
+		const float H = 16.f;
+		const float HSQ = H * H;
 		// assume all particles have the same mass
-		const static float MASS = 65.f;
+		const float MASS = 65.f;
 		// viscosity constant
-		const static float VISC = 700.f;
+		const float VISC = 700.f;
 		// timestep size
-		const static float DT = 0.001f;
+		const float DT = 0.001f;
 
 		// smoothing kernels and their gradients
-		const static float POLY6 = 315.f / (64.f * M_PI * pow(H, 9.f));
-		const static float SPIKY_GRAD = -45.f / (M_PI * pow(H, 6.f));
-		const static float VISC_LAP = 45.f / (M_PI * pow(H, 6.f));
+		const float POLY6 = 315.f / (64.f * M_PI * pow(H, 9.f));
+		const float SPIKY_GRAD = -45.f / (M_PI * pow(H, 6.f));
+		const float VISC_LAP = 45.f / (M_PI * pow(H, 6.f));
 
 		// number of particles on the edge of the square
-		const static int EDGE_NUM_PARTICLES = 32;
-		const static int PARTICLE_DEPTH = 4;
-		const static int NUM_PARTICLES = PARTICLE_DEPTH * EDGE_NUM_PARTICLES * EDGE_NUM_PARTICLES;
-		const static float PARTICLE_DIST = 5;
+		const int EDGE_NUM_PARTICLES = 32;
+		const int PARTICLE_DEPTH = 4;
+		const int NUM_PARTICLES = PARTICLE_DEPTH * EDGE_NUM_PARTICLES * EDGE_NUM_PARTICLES;
+		const float PARTICLE_DIST = 5;
 
 
 		// boundary parameters 
-		const static float EPS = H; // boundary epsilon
-		const static float BOUND_DAMPING = -.9f;
+		const float EPS = H; // boundary epsilon
+		const float BOUND_DAMPING = -.9f;
 
-		const static float BOUND = 100;
+		const float BOUND = 100;
 };
 
 #endif
