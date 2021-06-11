0cbd5381f1021b75b14a673e48e69f94e47fa3c2
Nate Hill <nateh777@cs.utexas.edu>
2021-06-11 16:56:59 -0500

    Remove const qualifier from simulation paramters in Fluid class

--- trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
+++ trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
@@ -22,38 +22,38 @@ class Fluid {
 		int default_data();
 		int update_SPH();
 		
-		const float G_STRENGTH = 600;
+		float G_STRENGTH = 600;
 		// external graviational force
-		const float G[3] = {0.f, G_STRENGTH * -9.8f, 0.f};
-		const float REST_DENS = 1000.f;
-		const float GAS_CONST = 100.f;
+		float G[3] = {0.f, G_STRENGTH * -9.8f, 0.f};
+		float REST_DENS = 1000.f;
+		float GAS_CONST = 100.f;
 		// kernel radius
-		const float H = 16.f;
-		const float HSQ = H * H;
+		float H = 16.f;
+		float HSQ = H * H;
 		// assume all particles have the same mass
-		const float MASS = 65.f;
+		float MASS = 65.f;
 		// viscosity constant
-		const float VISC = 700.f;
+		float VISC = 700.f;
 		// timestep size
-		const float DT = 0.001f;
+		float DT = 0.001f;
 
 		// smoothing kernels and their gradients
-		const float POLY6 = 315.f / (64.f * M_PI * std::pow(H, 9.f));
-		const float SPIKY_GRAD = -45.f / (M_PI * std::pow(H, 6.f));
-		const float VISC_LAP = 45.f / (M_PI * std::pow(H, 6.f));
+		float POLY6 = 315.f / (64.f * M_PI * std::pow(H, 9.f));
+		float SPIKY_GRAD = -45.f / (M_PI * std::pow(H, 6.f));
+		float VISC_LAP = 45.f / (M_PI * std::pow(H, 6.f));
 
 		// number of particles on the edge of the square
-		const int EDGE_NUM_PARTICLES = 32;
-		const int PARTICLE_DEPTH = 4;
-		const int NUM_PARTICLES = PARTICLE_DEPTH * EDGE_NUM_PARTICLES * EDGE_NUM_PARTICLES;
-		const float PARTICLE_DIST = 5;
+		int EDGE_NUM_PARTICLES = 32;
+		int PARTICLE_DEPTH = 4;
+		int NUM_PARTICLES = PARTICLE_DEPTH * EDGE_NUM_PARTICLES * EDGE_NUM_PARTICLES;
+		float PARTICLE_DIST = 5;
 
 
 		// boundary parameters 
-		const float EPS = H; // boundary epsilon
-		const float BOUND_DAMPING = -.9f;
+		float EPS = H; // boundary epsilon
+		float BOUND_DAMPING = -.9f;
 
-		const float BOUND = 100;
+		float BOUND = 100;
 };
 
 #endif
