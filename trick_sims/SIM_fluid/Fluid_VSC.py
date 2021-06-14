08071b3aca26d7bb065997862bbe97813337922c
Nate Hill <nateh777@cs.utexas.edu>
2021-06-14 14:37:52 -0500

    Implement simple 2D GUI in order to render a single fluid particle

--- trick_sims/SIM_fluid/Fluid_VSC.py
+++ trick_sims/SIM_fluid/Fluid_VSC.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import socket
+from tkinter import *
 
 # Process command line args
 if ( len(sys.argv) == 2) :
@@ -8,6 +9,17 @@ if ( len(sys.argv) == 2) :
 else :
 	print("Usage: vsclient <port_number>")
 	sys.exit()
+
+
+# Create the GUI
+tk = Tk()
+canvas = Canvas(tk, width=200, height=200)
+tk.title("SPH Display")
+canvas.pack()
+
+# Create oval objects to represent fluid particles
+particle_radius = 2;
+fluidParticle = canvas.create_oval(0, 0, particle_radius, particle_radius, fill="blue")
 
 # Connect to the variable server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,10 +32,17 @@ client_socket.send("trick.var_ascii()\n".encode())
 client_socket.send("trick.var_add(\"dyn.fluid.VISC\")\n".encode())
 client_socket.send("trick.var_unpause()\n".encode())
 
-# Unit test 1: request the position of a particle
+# Unit test 1: request the x position of a particle
 client_socket.send("trick.var_pause()\n".encode())
 client_socket.send("trick.var_ascii()\n".encode())
 client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[0].pos[0]\")\n".encode())
+client_socket.send("trick.var_unpause()\n".encode())
+
+
+client_socket.send("trick.var_pause()\n".encode())
+client_socket.send("trick.var_ascii()\n".encode())
+client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[16].pos[0]\")\n".encode())
+client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[16].pos[1]\")\n".encode())
 client_socket.send("trick.var_unpause()\n".encode())
 
 # Repeatedly read and process the responses from the variable server.
@@ -31,4 +50,17 @@ while (True):
 	line = insock.readline()
 	if line == '':
 		break
+	field = line.split("\t")
+	# Get position of particle and update it on the canvas
+	x = float(field[1])
+	y = float(field[2])
+	cx = x + 100
+	cy = 200 - (y + 100)
+	canvas.coords(fluidParticle, cx - particle_radius, cy - particle_radius, cx + particle_radius, cy+ particle_radius)
+	
 	print(line)
+	# Update the Tk graphics
+	tk.update()
+	
+# Keep the window open, when the data stops.
+tk.mainloop()
