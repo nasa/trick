f79770e360f5f5d543311ec3743de3eaf7ac221a
Nate Hill <nateh777@cs.utexas.edu>
2021-06-14 15:09:54 -0500

    Support 2D rendering of multiple particles in python variable client

--- trick_sims/SIM_fluid/Fluid_VSC.py
+++ trick_sims/SIM_fluid/Fluid_VSC.py
@@ -19,7 +19,9 @@ canvas.pack()
 
 # Create oval objects to represent fluid particles
 particle_radius = 2;
-fluidParticle = canvas.create_oval(0, 0, particle_radius, particle_radius, fill="blue")
+fluidParticles = []
+for i in range(1024):
+	fluidParticles.append(canvas.create_oval(0, 0, particle_radius, particle_radius, fill="blue"))
 
 # Connect to the variable server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,8 +43,9 @@ client_socket.send("trick.var_unpause()\n".encode())
 
 client_socket.send("trick.var_pause()\n".encode())
 client_socket.send("trick.var_ascii()\n".encode())
-client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[16].pos[0]\")\n".encode())
-client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[16].pos[1]\")\n".encode())
+for i in range(1024):
+	client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[{}].pos[0]\")\n".format(i).encode())
+	client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[{}].pos[1]\")\n".format(i).encode())
 client_socket.send("trick.var_unpause()\n".encode())
 
 # Repeatedly read and process the responses from the variable server.
@@ -52,11 +55,12 @@ while (True):
 		break
 	field = line.split("\t")
 	# Get position of particle and update it on the canvas
-	x = float(field[1])
-	y = float(field[2])
-	cx = x + 100
-	cy = 200 - (y + 100)
-	canvas.coords(fluidParticle, cx - particle_radius, cy - particle_radius, cx + particle_radius, cy+ particle_radius)
+	for i in range(1024):
+		x = float(field[2 * i + 1])
+		y = float(field[2 * i + 2])
+		cx = x + 100
+		cy = 200 - (y + 100)
+		canvas.coords(fluidParticles[i], cx - particle_radius, cy - particle_radius, cx + particle_radius, cy+ particle_radius)
 	
 	print(line)
 	# Update the Tk graphics
