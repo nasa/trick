e7fbf9caa7ea093c470ecb2df6de16f1cebf9d0d
Nate Hill <nateh777@cs.utexas.edu>
2021-06-14 16:54:27 -0500

    Start variable server client from input.py rather than from command line

--- trick_sims/SIM_fluid/RUN_test/input.py
+++ trick_sims/SIM_fluid/RUN_test/input.py
@@ -1,2 +1,13 @@
 exec(open("Modified_data/realtime.py").read())
-#trick.stop(5)
+
+#==================================
+# Start the variable server client.
+#==================================
+var_server_port = trick.var_server_get_port()
+fluid_graphics_path = os.environ['HOME'] + "/trick_fork/trick_sims/SIM_fluid/Fluid_VSC.py"
+if (os.path.isfile(fluid_graphics_path)) :
+	fluid_graphics_cmd = "python3 " + fluid_graphics_path + " " + str(var_server_port) + " &"
+	print(fluid_graphics_cmd)
+	os.system(fluid_graphics_cmd)
+else :
+	print('Oops! Can\'t find ' + fluid_graphics_path)
