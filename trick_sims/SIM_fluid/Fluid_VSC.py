36e7ac8e3f672ae11a7b901b95ccb49f29def9fa
Nate Hill <nateh777@cs.utexas.edu>
2021-06-14 12:13:34 -0500

    Create simple python variable server client to unit test connection by accessing VISC member of fluid

---
+++ trick_sims/SIM_fluid/Fluid_VSC.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+import socket
+
+# Process command line args
+if ( len(sys.argv) == 2) :
+	trick_varserver_port = int(sys.argv[1])
+else :
+	print("Usage: vsclient <port_number>")
+	sys.exit()
+
+# Connect to the variable server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect( ("localhost", trick_varserver_port) )
+insock = client_socket.makefile("r")
+
+# Unit test 0: request viscosity of the fluid
+client_socket.send("trick.var_pause()\n".encode())
+client_socket.send("trick.var_ascii()\n".encode())
+client_socket.send("trick.var_add(\"dyn.fluid.VISC\")\n".encode())
+client_socket.send("trick.var_unpause()\n".encode())
+
+# Repeatedly read and process the responses from the variable server.
+while (True):
+	line = insock.readline()
+	if line == '':
+		break
+	print(line)
