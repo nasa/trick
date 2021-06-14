10b64824b218257841f5112047e461d27ad33c06
Nate Hill <nateh777@cs.utexas.edu>
2021-06-14 13:29:10 -0500

    Create unit test to see if particle position is being updated correctly

--- trick_sims/SIM_fluid/Fluid_VSC.py
+++ trick_sims/SIM_fluid/Fluid_VSC.py
@@ -20,6 +20,12 @@ client_socket.send("trick.var_ascii()\n".encode())
 client_socket.send("trick.var_add(\"dyn.fluid.VISC\")\n".encode())
 client_socket.send("trick.var_unpause()\n".encode())
 
+# Unit test 1: request the position of a particle
+client_socket.send("trick.var_pause()\n".encode())
+client_socket.send("trick.var_ascii()\n".encode())
+client_socket.send("trick.var_add(\"dyn.fluid.particlesArr[0].pos[0]\")\n".encode())
+client_socket.send("trick.var_unpause()\n".encode())
+
 # Repeatedly read and process the responses from the variable server.
 while (True):
 	line = insock.readline()
