9341adf3c6404ebdb743076d579a478fd09df94d
Nate Hill <nateh777@cs.utexas.edu>
2021-06-21 10:25:38 -0500

    Add CUDA runtime library flag and include directories

--- trick_sims/SIM_fluid/S_overrides.mk
+++ trick_sims/SIM_fluid/S_overrides.mk
@@ -1,2 +1,3 @@
-TRICK_CFLAGS += -Imodels
-TRICK_CXXFLAGS += -Imodels
+TRICK_CFLAGS += -Imodels -I/usr/local/cuda-11.0/include
+TRICK_CXXFLAGS += -Imodels -I/usr/local/cuda-11.0/include
+TRICK_USER_LINK_LIBS += -lcudart
