9802fb37c67033b180aff0f73c5b4fd7069f8e6f
Nate Hill <nateh777@cs.utexas.edu>
2021-06-23 11:59:56 -0500

    Successfully compile and link Trick with CUDA in S_overrides.mk

--- trick_sims/SIM_fluid/S_overrides.mk
+++ trick_sims/SIM_fluid/S_overrides.mk
@@ -1,3 +1,19 @@
-TRICK_CFLAGS += -Imodels -I/usr/local/cuda-11.0/include
-TRICK_CXXFLAGS += -Imodels -I/usr/local/cuda-11.0/include
-TRICK_USER_LINK_LIBS += -lcudart
+TRICK_CFLAGS += -Imodels -I/usr/local/cuda/include
+TRICK_CXXFLAGS += -Imodels -I/usr/local/cuda/include
+TRICK_USER_LINK_LIBS += models/fluid/src/gpuCode.o models/fluid/src/vectorAdd.o -L/usr/local/cuda/lib64 -lcudart -lcudadevrt
+
+$(S_MAIN): models/fluid/src/gpuCode.o
+
+models/fluid/src/vectorAdd.o: models/fluid/src/vectorAdd.cu
+	nvcc -arch=sm_35 -I. -dc models/fluid/src/vectorAdd.cu -o models/fluid/src/vectorAdd.o
+
+models/fluid/src/gpuCode.o: models/fluid/src/vectorAdd.o
+	nvcc -arch=sm_35 -dlink models/fluid/src/vectorAdd.o $(S_OBJECTS) -o models/fluid/src/gpuCode.o
+	#nvcc -arch=sm_35 -dlink $(TRICK_SYSTEM_LDFLAGS) $(S_OBJECTS) $(LINK_LISTS) $(TRICK_LDFLAGS) $(TRICK_USER_LINK_LIBS) $(READ_ONLY_LIBS) $(LD_WHOLE_ARCHIVE) $(TRICK_LIBS) $(LD_NO_WHOLE_ARCHIVE) $(TRICK_EXEC_LINK_LIBS)
+	
+clean: my_clean
+	
+	
+my_clean:
+	-rm -rf models/fluid/src/vectorAdd.o models/fluid/src/gpuCode.o
+	
