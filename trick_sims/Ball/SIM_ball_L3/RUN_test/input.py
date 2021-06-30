35a2c91c866e30dd699fb6bb62fcb80cc762b05e
Penn, John M 047828115 <john.m.penn@nasa.gov>
2021-06-30 11:44:58 -0500

    Fix SIM_ball_L3 by integrating an ensemble state. #725

--- trick_sims/Ball/SIM_ball_L3/RUN_test/input.py
+++ trick_sims/Ball/SIM_ball_L3/RUN_test/input.py
@@ -1,10 +1,7 @@
-
 exec(open("Modified_data/data_record.dr").read())
 exec(open("Modified_data/auto_test.dr").read())
 
-ball.altimeter.input.add_noise = False
+ball1.altimeter.input.add_noise = False
 ball2.altimeter.input.add_noise = False
-
-my_integ_loop.getIntegrator(trick.Runge_Kutta_Fehlberg_78, 4)
 
 trick.stop(300.0)
