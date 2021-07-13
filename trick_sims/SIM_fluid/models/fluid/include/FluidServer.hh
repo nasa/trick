bf440c975d5dd13cf1a084abbc0d7084918108d0
Nate Hill <nateh777@cs.utexas.edu>
2021-07-13 13:25:39 -0500

    Add Trick Header to FluidServer.hh and add clientfd member to FluidServer

--- trick_sims/SIM_fluid/models/fluid/include/FluidServer.hh
+++ trick_sims/SIM_fluid/models/fluid/include/FluidServer.hh
@@ -1,8 +1,15 @@
-#include "Fluid.hh"
+/********************************* TRICK HEADER *******************************
+PURPOSE: (Create a TCP server to transmit particle data to client)
+*******************************************************************************/
+#ifndef _fluidserver_hh_
+#define _fluidserver_hh_
 
 class FluidServer {
+    private:
+        int clientfd;
     public:
-        FluidServer();
-        int initConnection();
-        void sendParticleData(int clientfd, Fluid* fluid);
-}
+        void initConnection();
+        void sendParticleData();
+};
+
+#endif
