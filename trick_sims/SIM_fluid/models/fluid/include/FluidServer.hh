a3e9926d016a56def2c5900ab71c567b97e0e84c
Nate Hill <nateh777@cs.utexas.edu>
2021-07-14 16:24:44 -0500

    Pass fluid pointer as parameter to FluidServer::sendParticleData(Fluid* fluid)

--- trick_sims/SIM_fluid/models/fluid/include/FluidServer.hh
+++ trick_sims/SIM_fluid/models/fluid/include/FluidServer.hh
@@ -3,13 +3,14 @@ PURPOSE: (Create a TCP server to transmit particle data to client)
 *******************************************************************************/
 #ifndef _fluidserver_hh_
 #define _fluidserver_hh_
+#include "Fluid.hh"
 
 class FluidServer {
     private:
         int clientfd;
     public:
         void initConnection();
-        void sendParticleData();
+        void sendParticleData(Fluid* fluid);
 };
 
 #endif
