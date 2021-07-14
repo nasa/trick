9a7ff1569c17badcc0ebea4fcec093e0ba5bbeb4
Nate Hill <nateh777@cs.utexas.edu>
2021-07-14 18:47:49 -0500

    Change openGLCaller signature

--- trick_sims/SIM_fluid/models/fluid/graphics/src/opengl_caller.cpp
+++ trick_sims/SIM_fluid/models/fluid/graphics/src/opengl_caller.cpp
@@ -2,15 +2,9 @@
 
 extern int openGLMain(int argc, char* argv[]);
 
-void openGLCaller() {
-    /*int argc = 2;
-    char* argv[] = {"./sim", "../100_sphere.obj"};
+void openGLCaller(int argc, char* argv[]) {
     {
         std::thread openGLThread(openGLMain, argc, argv);
         openGLThread.detach();
-    }*/
-    int x = 0;
-    for (int i = 0; i < 10000000000; i++) {
-        x+= i;
     }
 }
