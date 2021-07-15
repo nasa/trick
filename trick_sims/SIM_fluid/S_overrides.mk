9f95301f84408aa8140f5277ef46d8d1be0a228a
Nate Hill <nateh777@cs.utexas.edu>
2021-07-15 16:13:53 -0500

    Fix compilation bug with g++ and add .o files to nvcc device linking and TRICK_USER_LINK_LIBS

--- trick_sims/SIM_fluid/S_overrides.mk
+++ trick_sims/SIM_fluid/S_overrides.mk
@@ -1,20 +1,20 @@
 TRICK_CFLAGS += -Imodels -I/usr/local/cuda/include
 TRICK_CXXFLAGS += -Imodels -I/usr/local/cuda/include
-TRICK_USER_LINK_LIBS += models/fluid/src/gpuCode.o models/fluid/src/sph_gpu.o -L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lglfw -lGL -lGLEW -pthread
+TRICK_USER_LINK_LIBS += models/fluid/src/gpuCode.o models/fluid/src/sph_gpu.o models/fluid/graphics/src/opengl_caller.o models/fluid/graphics/src/opengl.o models/fluid/graphics/src/gui.o models/fluid/graphics/src/sph.o -L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lglfw -lGL -lGLEW -pthread
 
 $(S_MAIN): models/fluid/graphics/src/opengl_caller.o models/fluid/graphics/src/opengl.o models/fluid/graphics/src/gui.o models/fluid/graphics/src/sph.o
 
 models/fluid/graphics/src/opengl_caller.o: models/fluid/graphics/src/opengl_caller.cpp
-	g++ -c models/fluid/graphics/src/opengl_caller.cpp
+	g++ -c models/fluid/graphics/src/opengl_caller.cpp -o models/fluid/graphics/src/opengl_caller.o
 
 models/fluid/graphics/src/opengl.o: models/fluid/graphics/src/opengl.cc
-	g++ -c models/fluid/graphics/src/opengl.cpp
+	g++ -c models/fluid/graphics/src/opengl.cc -o models/fluid/graphics/src/opengl.o
 
 models/fluid/graphics/src/gui.o: models/fluid/graphics/src/gui.cc
-	g++ -c models/fluid/graphics/src/gui.cpp
+	g++ -c models/fluid/graphics/src/gui.cc -o models/fluid/graphics/src/gui.o
 
 models/fluid/graphics/src/sph.o: models/fluid/graphics/src/sph.cpp
-	g++ -c models/fluid/graphics/src/sph.cpp
+	g++ -c models/fluid/graphics/src/sph.cpp -o models/fluid/graphics/src/sph.o
 
 $(S_MAIN): models/fluid/src/gpuCode.o
 
@@ -22,7 +22,7 @@ models/fluid/src/sph_gpu.o: models/fluid/src/sph_gpu.cu
 	nvcc -arch=sm_35 -I. -dc models/fluid/src/sph_gpu.cu -o models/fluid/src/sph_gpu.o
 
 models/fluid/src/gpuCode.o: models/fluid/src/sph_gpu.o
-	nvcc -arch=sm_35 -dlink models/fluid/src/sph_gpu.o $(S_OBJECTS) -o models/fluid/src/gpuCode.o
+	nvcc -arch=sm_35 -dlink models/fluid/src/sph_gpu.o models/fluid/graphics/src/opengl_caller.o models/fluid/graphics/src/opengl.o models/fluid/graphics/src/gui.o models/fluid/graphics/src/sph.o $(S_OBJECTS) -o models/fluid/src/gpuCode.o
 	#nvcc -arch=sm_35 -dlink $(TRICK_SYSTEM_LDFLAGS) $(S_OBJECTS) $(LINK_LISTS) $(TRICK_LDFLAGS) $(TRICK_USER_LINK_LIBS) $(READ_ONLY_LIBS) $(LD_WHOLE_ARCHIVE) $(TRICK_LIBS) $(LD_NO_WHOLE_ARCHIVE) $(TRICK_EXEC_LINK_LIBS)
 	
 clean: my_clean
