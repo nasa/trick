97a1db6c6de03e88b3a00cda390770ab216a8c79
Nate Hill <nateh777@cs.utexas.edu>
2021-07-14 19:47:11 -0500

    Temporarily set argc and argv in openGLCaller and change signature of openGLCaller

--- trick_sims/SIM_fluid/models/fluid/graphics/src/opengl_caller.cpp
+++ trick_sims/SIM_fluid/models/fluid/graphics/src/opengl_caller.cpp
@@ -2,9 +2,13 @@
 
 extern int openGLMain(int argc, char* argv[]);
 
-void openGLCaller(int argc, char* argv[]) {
+void openGLCaller() {
+    int argc = 2;
+    char* argv[] = {"./sim", "../100_sphere.obj"};
+    
     {
         std::thread openGLThread(openGLMain, argc, argv);
         openGLThread.detach();
     }
+    
 }
