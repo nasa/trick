bdd766ee477a59fff3557476283804ca24c29e20
Nate Hill <nateh777@cs.utexas.edu>
2021-07-29 13:01:03 -0500

    Add constants for spatial grid in Fluid.hh

--- trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
+++ trick_sims/SIM_fluid/models/fluid/include/Fluid.hh
@@ -15,7 +15,8 @@ class Fluid {
 		void verletUpdatePosition(int p_start, int p_end);
 		void timeIntegration(int p_start, int p_end);
 		void buildSpatialGrid();
-		std::vector<Particle> getCandidateNeighbors(float x, float y, float z);
+		std::vector<Particle> getNeighbors(int grid_x, int grid_y, int grid_z);
+		bool checkBounds(int gridX, int gridY, int gridZ);
 
 
 	public:
@@ -34,7 +35,7 @@ class Fluid {
 		float REST_DENS = 1000.f;
 		float GAS_CONST = 100.f;
 		// kernel radius
-		float H = 16.f;
+		float H = 15.f;
 		float HSQ = H * H;
 		// assume all particles have the same mass
 		float MASS = 65.f;
@@ -49,8 +50,8 @@ class Fluid {
 		float VISC_LAP = 45.f / (M_PI * std::pow(H, 6.f));
 
 		// number of particles on the edge of the square
-		int EDGE_NUM_PARTICLES = 32;
-		int PARTICLE_DEPTH = 2;
+		int EDGE_NUM_PARTICLES = 20;
+		int PARTICLE_DEPTH = 4;
 		int NUM_PARTICLES = PARTICLE_DEPTH * EDGE_NUM_PARTICLES * EDGE_NUM_PARTICLES;
 		float PARTICLE_DIST = 5;
 
@@ -60,7 +61,11 @@ class Fluid {
 		float EPS = H; // boundary epsilon
 		float BOUND_DAMPING = -.9f;
 
-		float BOUND = 300;
+		float BOUND = 200;
+
+		// parameters for spatial grid
+		int CELL_SIZE = 2 * H;
+		int CELLS_PER_DIM = 2 * BOUND / CELL_SIZE;
 };
 
 #endif
