c14fbfa6e517176e37041e3bbceb42505e602bc2
Caleb Herpin <caleb.herpin@gmail.com>
2021-07-29 22:49:15 -0500

    added a 32 bit civet test

--- trick_sims/Cannon/SIM_cannon_webserver/RUN_test/input.py
+++ trick_sims/Cannon/SIM_cannon_webserver/RUN_test/input.py
@@ -7,7 +7,7 @@ web.server.ssl_enable = False
 #web.server.path_to_ssl_cert = "server.pem"
 web.server.error_log_file = "log.error"
 
-web.server.port = 9000
+web.server.port = 8888
 trick.var_server_set_port(9001)
 
 exec(open("Modified_data/realtime.py").read())
