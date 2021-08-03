c226c352c6ee5dfb2d8687195f3519d3da5be0e0
Caleb Herpin <caleb.herpin@gmail.com>
2021-08-03 00:28:39 -0500

    Default to freeze for webserver sim example

--- trick_sims/Cannon/SIM_cannon_webserver/Modified_data/realtime.py
+++ trick_sims/Cannon/SIM_cannon_webserver/Modified_data/realtime.py
@@ -5,7 +5,7 @@ trick.exec_set_software_frame(0.1)
 trick.itimer_enable()
 
 #trick.exec_set_enable_freeze(True)
-#trick.exec_set_freeze_command(True)
+trick.exec_set_freeze_command(True)
 
 #simControlPanel = trick.SimControlPanel()
 #trick.add_external_application(simControlPanel)
