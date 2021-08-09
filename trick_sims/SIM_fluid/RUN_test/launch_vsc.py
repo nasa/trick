063c2dcd149eaeba4d4b6c816f48955f646f0592
Nate Hill <nateh777@cs.utexas.edu>
2021-08-09 17:13:43 -0500

    Change environment specific path to relative path in launch_vsc.py

--- trick_sims/SIM_fluid/RUN_test/launch_vsc.py
+++ trick_sims/SIM_fluid/RUN_test/launch_vsc.py
@@ -4,7 +4,8 @@ exec(open("Modified_data/realtime.py").read())
 # Start the variable server client.
 #==================================
 var_server_port = trick.var_server_get_port()
-fluid_graphics_path = os.environ['HOME'] + "/trick_fork/trick_sims/SIM_fluid/Fluid_VSC.py"
+
+fluid_graphics_path = "Fluid_VSC.py"
 if (os.path.isfile(fluid_graphics_path)) :
 	fluid_graphics_cmd = "python3 " + fluid_graphics_path + " " + str(var_server_port) + " &"
 	print(fluid_graphics_cmd)
