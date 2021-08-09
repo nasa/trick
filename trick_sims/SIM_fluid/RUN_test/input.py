38bb0f04495500ba7e714f66293b24097bd8416d
Nate Hill <nateh777@cs.utexas.edu>
2021-08-09 17:36:10 -0500

    Add comments for demos and reduce usage of magic numbers in input.py

--- trick_sims/SIM_fluid/RUN_test/input.py
+++ trick_sims/SIM_fluid/RUN_test/input.py
@@ -1,32 +1,42 @@
 import math
 exec(open("Modified_data/realtime.py").read())
-dyn.fluid.BOUND = 380
+dyn.fluid.BOUND = 300
 dyn.fluid.ISO_RADIUS = 32
 
-for i in range(1600):
-    tx = i / 40 - 20
-    tz = i % 40 - 20
-    dyn.fluid.particlesArr[i].pos[0] = tx * 4
-    dyn.fluid.particlesArr[i].pos[2] = tz * 4 
-    dyn.fluid.particlesArr[i].pos[1] = tx * tx + tz * tz
+mode = -1
 
-"""
-for i in range(800):
-    t = i / 2
-    dyn.fluid.particlesArr[i].pos[1] = 15 * math.cos(math.radians(t))
-"""
-"""
-for i in range(800):
-    theta = (360 / 400) * i
-    print(theta)
-    dyn.fluid.particlesArr[i].pos[0] = 100 * math.cos(math.radians(theta))
-    dyn.fluid.particlesArr[i].pos[1] = 100 * math.sin(math.radians(theta))
-    if i > 400 and i <= 700:
-        theta = (360 / 300) * i
-        dyn.fluid.particlesArr[i].pos[0] = 70 * math.cos(math.radians(theta))
-        dyn.fluid.particlesArr[i].pos[1] = 70 * math.sin(math.radians(theta))
-    if i > 700:
-        theta = (360 / 100) * i
-        dyn.fluid.particlesArr[i].pos[0] = 40 * math.cos(math.radians(theta))
-        dyn.fluid.particlesArr[i].pos[1] = 40 * math.sin(math.radians(theta))
-"""
+
+if mode == 0:
+    # paraboloid initial condition
+    for i in range(dyn.fluid.NUM_PARTICLES):
+        ROOT_NUM_PARTICLES = math.sqrt(dyn.fluid.NUM_PARTICLES)
+        tx = i / ROOT_NUM_PARTICLES - (ROOT_NUM_PARTICLES / 2)
+        tz = i % ROOT_NUM_PARTICLES - (ROOT_NUM_PARTICLES / 2)
+        dyn.fluid.particlesArr[i].pos[0] = tx * 4
+        dyn.fluid.particlesArr[i].pos[2] = tz * 4 
+        dyn.fluid.particlesArr[i].pos[1] = tx * tx + tz * tz
+if mode == 1:
+    # cosine wave initial condition
+    for i in range(dyn.fluid.NUM_PARTICLES):
+        t = i / 2
+        dyn.fluid.particlesArr[i].pos[1] = 15 * math.cos(math.radians(t))
+
+
+
+elif mode == 2:
+    dyn.fluid.NUM_PARTICLES = 800
+    n = dyn.fluid.NUM_PARTICLES
+    # concentric circles initial conditions
+    for i in range(n):
+        theta = (360 / (n / 2)) * i
+        print(theta)
+        dyn.fluid.particlesArr[i].pos[0] = 100 * math.cos(math.radians(theta))
+        dyn.fluid.particlesArr[i].pos[1] = 100 * math.sin(math.radians(theta))
+        if i > n / 2 and i <= (3 / 4) * n:
+            theta = (360 / ((3 / 8) * n)) * i
+            dyn.fluid.particlesArr[i].pos[0] = 70 * math.cos(math.radians(theta))
+            dyn.fluid.particlesArr[i].pos[1] = 70 * math.sin(math.radians(theta))
+        if i > (7 / 8) * n:
+            theta = (360 / ((1 / 8) * n)) * i
+            dyn.fluid.particlesArr[i].pos[0] = 40 * math.cos(math.radians(theta))
+            dyn.fluid.particlesArr[i].pos[1] = 40 * math.sin(math.radians(theta))
