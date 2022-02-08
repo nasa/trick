0d60ef0b6faadc452007292770e776c01f1ef299
Scott Fennell <spfennell@gmail.com>
2022-02-08 09:59:26 -0600

    iss #1219 #1223 #1224 reference ignored in name from address (#1222)
    * #1219 added code to check reference mod bit in ClassicCheckPointAgent::getCompositeSubReference, use reference width instead of size.
    
    * #1219 refactor getCompositeSubReference for readability
    
    * #1219 and #1223 add reference width to mod bits in ICG
    
    * check for mod bits in TRICK_STRUCTURED section, refactor function
    
    * #1219 adding MM_ref_name_from_address tests
    
    * #1224 fix logic for offset into arrayed struct
    
    closes #1219 closes #1223 closes #1224 
    
    Co-authored-by: Fennell, Scott P 263712616 <scott.p.fennell@nasa.gov>

--- trick_source/sim_services/MemoryManager/test/MM_ref_name_from_address.cc
+++ trick_source/sim_services/MemoryManager/test/MM_ref_name_from_address.cc
@@ -54,3 +54,73 @@ TEST_F(MM_ref_name_from_address, test4) {
   EXPECT_EQ("&udt2_var2[1].udt1_var.udt_p", ref_name);
 
 }
+
+#if __GNUC__ > 4
+TEST_F(MM_ref_name_from_address, test5) {
+  int i;
+  double d;
+  UDT3 udt3_st = {i, false, d};
+  UDT3* udt3_p1 = &udt3_st;
+
+  memmgr->declare_extern_var(udt3_p1, "UDT3 udt3_st");
+  std::string ref_name = memmgr->ref_name_from_address(&(udt3_p1->bl));
+  EXPECT_EQ("&udt3_st.bl", ref_name);
+
+}
+
+TEST_F(MM_ref_name_from_address, test6) {
+  int i;
+  double d;
+  bool b;
+  UDT2 udt2_st;
+  UDT3 udt3_st = {i, false, d};
+  UDT4 udt4_st = {udt3_st, &b, udt2_st, 2.0};
+  UDT4 * udt4_p1 = & udt4_st;
+
+  memmgr->declare_extern_var(udt4_p1, "UDT4 udt4_st");
+  std::string ref_name = memmgr->ref_name_from_address(udt4_p1);
+  EXPECT_EQ("&udt4_st.udt3_ref", ref_name);
+
+}
+
+
+TEST_F(MM_ref_name_from_address, test7) {
+  int i;
+  double d;
+  bool b;
+  UDT2 udt2_st;
+  UDT3 udt3_st = {i, false, d};
+  UDT4 udt4_st = {udt3_st, &b, udt2_st, 2.0};
+  UDT4 * udt4_p1 = & udt4_st;
+
+  memmgr->declare_extern_var(udt4_p1, "UDT4 udt4_st");
+  std::string ref_name = memmgr->ref_name_from_address(&(udt4_p1->bl_p));
+  EXPECT_EQ("&udt4_st.bl_p", ref_name);
+
+}
+
+TEST_F(MM_ref_name_from_address, test8) {
+  int i;
+  double d;
+  bool b;
+  UDT2 udt2_st;
+  UDT3 udt3_st = {i, false, d};
+  UDT4 udt4_st = {udt3_st, &b, udt2_st, 2.0};
+  UDT4 * udt4_p1 = & udt4_st;
+
+  memmgr->declare_extern_var(udt4_p1, "UDT4 udt4_st");
+  std::string ref_name = memmgr->ref_name_from_address(&(udt4_p1->dbl));
+  EXPECT_EQ("&udt4_st.dbl", ref_name);
+
+}
+#endif
+
+TEST_F(MM_ref_name_from_address, test9) {
+
+  UDT5 udt5_st;
+
+  UDT5* udt5_p1 = (UDT5*)memmgr->declare_var("UDT5 udt5_var");
+  std::string ref_name = memmgr->ref_name_from_address(&(udt5_p1->udt2_2d_arr[2][1].x));
+  EXPECT_EQ("&udt5_var.udt2_2d_arr[2][1].x", ref_name);
+
+}
