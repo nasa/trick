92d33701daaf10bf73d2cf5e251cd95f6d5698be
Zero Nelson <jacaylanelson2002@gmail.com>
2022-07-18 14:19:38 -0500

    Add load/dump checkpoint button

--- trick_sims/SIM_webpanel/RUN_test/input.py
+++ trick_sims/SIM_webpanel/RUN_test/input.py
@@ -2,7 +2,7 @@ exec(open("Modified_data/realtime.py").read())
 web.server.enable = True
 web.server.port = 8888
 web.server.document_root = "www"
-web.server.debug = False
+web.server.debug = True
 web.server.ssl_enable = False
 web.server.error_log_file = "civet_server_error.log"
 
@@ -20,6 +20,7 @@ dyn.balloon.wind_speed = 0.0
 varServerPort = trick.var_server_get_port();
 BalloonDisplay_path = "models/graphics/dist/BalloonDisplay.jar"
 
+
 if (os.path.isfile(BalloonDisplay_path)) :
     BalloonDisplay_cmd = "java -jar " \
                    + BalloonDisplay_path \
@@ -29,4 +30,22 @@ if (os.path.isfile(BalloonDisplay_path)) :
 else :
     print('==================================================================================')
     print('BalloonDisplay needs to be built. Please \"cd\" into ../models/graphics and type \"make\".')
+    print('==================================================================================')
+
+# ==========================================
+# Start the Browser Client
+# ==========================================
+
+
+BrowserClient_path = "www/src/wsexp.html"
+
+if (os.path.isfile(BrowserClient_path)) :
+    BrowserClient_cmd = "firefox " \
+                    + BrowserClient_path \
+                    + " "   + "&";
+    print(BrowserClient_cmd)
+    os.system(BrowserClient_cmd);
+else :
+    print('==================================================================================')
+    print('Unable to open web panel. Please update the browser client path in input file')
     print('==================================================================================')
