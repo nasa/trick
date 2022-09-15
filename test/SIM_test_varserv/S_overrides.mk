25b8f7c864b22e7f59cabfeb5d07327461950b92
Jacqueline Deans <jdeans289@gmail.com>
2022-09-15 15:38:45 -0500

    Add c++11 flag to s_overrides for test_client

--- test/SIM_test_varserv/S_overrides.mk
+++ test/SIM_test_varserv/S_overrides.mk
@@ -5,10 +5,10 @@ TRICK_CXXFLAGS += -I./models
 all: test_client
 clean: clean_test_client
 
-TEST_CLIENT_LIBS := -lpthread -lgtest -lgtest_main
+TEST_CLIENT_LIBS += -L${GTEST_HOME}/lib64 -L${GTEST_HOME}/lib -lgtest -lgtest_main -lpthread
 
 test_client: models/test_client/test_client.cpp 
-	cd models/test_client; $(TRICK_CXX) test_client.cpp -o test_client $(TEST_CLIENT_LIBS)
+	cd models/test_client; $(TRICK_CXX) test_client.cpp -o test_client $(TEST_CLIENT_LIBS) -std=c++11
 
 clean_test_client:
 	rm -f models/test_client/test_client
