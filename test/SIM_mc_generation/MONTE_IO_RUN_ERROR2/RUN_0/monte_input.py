8cceb8f1035ded04afc2340dff7110901092d5ac
Isaac Reaves <IsaacDKReaves@Gmail.com>
2022-11-22 11:31:51 -0600

    More MCTrickOps Changes

--- test/SIM_mc_generation/MONTE_IO_RUN_ERROR2/RUN_0/monte_input.py
+++ test/SIM_mc_generation/MONTE_IO_RUN_ERROR2/RUN_0/monte_input.py
@@ -4,4 +4,4 @@ monte_carlo.mc_master.generate_dispersions = False
 exec(open('IO_RUN_ERROR2/input.py').read())
 monte_carlo.mc_master.monte_run_number = 0
 
-monte_carlo.x_file_lookup[0] = 2
+test.x_file_lookup[0] = 2
