ae51bc634f05588ded1f435bcfb32c8bf5d5be40
Isaac Reaves <IsaacDKReaves@Gmail.com>
2022-11-23 14:28:52 -0600

    TrickWorkFlow Deleted

--- test/SIM_mc_generation/verif_data/MONTE_RUN_WARN_config_error/RUN_0/monte_input.py
+++ test/SIM_mc_generation/verif_data/MONTE_RUN_WARN_config_error/RUN_0/monte_input.py
@@ -4,4 +4,4 @@ monte_carlo.mc_master.generate_dispersions = False
 exec(open('RUN_WARN_config_error/input.py').read())
 monte_carlo.mc_master.monte_run_number = 0
 
-monte_carlo.x_normal = 11.33418255640549
+test.x_normal = 11.33418255640549
