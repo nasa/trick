a920c4a09be483e02532a8cf42aefd667a2cd669
Jacqueline Deans <jdeans289@gmail.com>
2022-11-23 14:25:10 -0600

    Update trickops to run varserver test

--- test/SIM_test_varserv/RUN_test/unit_test.py
+++ test/SIM_test_varserv/RUN_test/unit_test.py
@@ -11,15 +11,12 @@ def main():
 	trick.exec_set_software_frame(0.01)
 	# trick.set_var_server_info_msg_on()
 
-
-	# This won't pass, since the port doesn't actually get set until initialization jobs which are run after this
-	# TRICK_EXPECT_EQ(trick.var_server_get_port(), 40000, "VariableServerTest", "SetPortNumber")
-
 	trick.exec_set_terminate_time(5.0)
 
 	varServerPort = trick.var_server_get_port()
 	test_output = ( os.getenv("TRICK_HOME") + "/trick_test/SIM_test_varserv.xml" ) 
 
+	# Start the test client
 	os.system("./models/test_client/test_client " + str(varServerPort) + " --gtest_output=xml:" + test_output + " &")
 
 
