3d70256b89692818c1139a90730eb868b26ccc7f
Jacqueline Deans <jdeans289@gmail.com>
2022-12-20 12:57:40 -0600

    Add integration test for output redirection

---
+++ test/SIM_test_output_dir/models/starter.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+PURPOSE: (Starter class)
+LIBRARY DEPENDENCY:
+    (
+    (starter.cpp)
+    )
+**************************************************************************/
+
+#include "dynamic_obj.h"
+
+class Starter {
+public:
+    Starter();
+    int default_data();
+    int init();
+    int scheduled();
+    int deriv();
+    int integ();
+    int shutdown();
+
+};
