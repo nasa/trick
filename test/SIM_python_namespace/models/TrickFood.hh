8821bac18a2a9b1c08706e1e428edc2ffb20abf6
Jacqueline Deans <jdeans289@gmail.com>
2023-01-23 08:50:13 -0600

    Handle empty PYTHON_MODULE in header comment (#1432)

---
+++ test/SIM_python_namespace/models/TrickFood.hh
@@ -0,0 +1,32 @@
+/**
+@file
+
+@verbatim
+PURPOSE: (Test that an empty PYTHON_MODULE won't cause errors or have any effect.)
+PYTHON_MODULE: ()
+@endverbatim
+*******************************************************************************/
+
+#ifndef TRICKFOOD_HH
+#define TRICKFOOD_HH
+
+#include <iostream>
+
+
+
+enum Fast {
+    Pizza,
+    Burger,
+    Taco
+};
+
+class Food {
+  public:
+    Food() : fast(Taco) {}
+    void print_me() { std::cout << "Food::print_me!" << std::endl; }
+    Fast fast;
+};
+
+
+#endif
+
