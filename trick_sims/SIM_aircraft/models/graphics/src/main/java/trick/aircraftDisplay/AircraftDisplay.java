cce3ffa946163f3c2aafae443b9e4e02e380d648
Marcus Rockwell <marcusrockwelljr@gmail.com>
2023-01-25 03:09:58 -0600

    Updating the build process to use Maven

--- trick_sims/SIM_aircraft/models/graphics/src/main/java/trick/aircraftDisplay/AircraftDisplay.java
+++ trick_sims/SIM_aircraft/models/graphics/src/main/java/trick/aircraftDisplay/AircraftDisplay.java
@@ -3,7 +3,7 @@
  * 2016 (c) National Aeronautics and Space Administration (NASA)
  */
 
-package trick;
+/* package main.java.trick.aircraftDisplay; */
 
 import java.awt.Graphics2D;
 import java.awt.Graphics;
