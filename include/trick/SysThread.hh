194841969ff14e92e6100c352df632ed0e66e88f
Deans <jdeans289@gmail.com>
2023-02-08 17:54:11 -0600

    Add comment [no ci]

--- include/trick/SysThread.hh
+++ include/trick/SysThread.hh
@@ -24,6 +24,8 @@ namespace Trick {
     /**
      * The purpose of this class is to ensure safe shutdown for Trick system threads, since user threads are handled separately in
      * the Trick::Threads and Executive classes.
+     * 
+     * This class was implemented as a solution to issue https://github.com/nasa/trick/issues/1445
      *
      * @author Jackie Deans
      *
