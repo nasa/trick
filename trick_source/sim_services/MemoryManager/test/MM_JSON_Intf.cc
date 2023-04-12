a5e31d43b121ee1d7678d51da8a6fadc191d69d1
John M. Penn <john.m.penn@nasa.gov>
2023-04-12 16:45:30 -0500

    Tweak MM_JSON_Intf.cc to get better code-coverage.

--- trick_source/sim_services/MemoryManager/test/MM_JSON_Intf.cc
+++ trick_source/sim_services/MemoryManager/test/MM_JSON_Intf.cc
@@ -29,111 +29,111 @@ class MM_JSON_intf : public ::testing::Test {
 /*
      The tests.
  */
+TEST_F(MM_JSON_intf, Write_Named_Local_C_Alloc_Info ) {
+    std::stringstream ss;
+    std::string s;
 
- TEST_F(MM_JSON_intf, Write_Named_Local_C_Alloc_Info ) {
-     std::stringstream ss;
-     std::string s;
+    double *d_p = (double*)memmgr->declare_var("double foo[2]");
+    ALLOC_INFO* alloc_info = memmgr->get_alloc_info_of(d_p);
+    memmgr->write_JSON_alloc_info(ss, alloc_info);
+    s = ss.str();
 
-     double *d_p = (double*)memmgr->declare_var("double foo");
-     ALLOC_INFO* alloc_info = memmgr->get_alloc_info_of(d_p);
-     memmgr->write_JSON_alloc_info(ss, alloc_info);
-     s = ss.str();
+    std::size_t pos;
+    bool found = true;
 
-     std::size_t pos;
-     bool found = true;
-
-     pos = s.find("name");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("foo");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("start");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("end");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("num");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("size");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("type");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("stcl");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("TRICK_LOCAL");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("language");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("Language_C");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("index");
-     if (pos == std::string::npos) found = false;
-     EXPECT_EQ(found, true);
- }
+    pos = s.find("name");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("foo");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("start");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("end");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("num");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("size");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("type");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("stcl");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("TRICK_LOCAL");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("language");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("Language_C");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("index");
+    if (pos == std::string::npos) found = false;
+    EXPECT_EQ(found, true);
+}
 
  TEST_F(MM_JSON_intf, Write_Anon_Extern_CPP_Alloc_Info ) {
-     std::stringstream ss;
-     std::string s;
+    std::stringstream ss;
+    std::string s;
 
-     UDT1 test_var;
-     UDT1 *test_var_p = (UDT1 *)memmgr->declare_extern_var(&test_var, "UDT1");
-     ALLOC_INFO* alloc_info = memmgr->get_alloc_info_of(test_var_p);
-     //memmgr->write_JSON_alloc_info(std::cout, alloc_info);
-     memmgr->write_JSON_alloc_info(ss, alloc_info);
-     s = ss.str();
+    UDT1 test_var;
+    UDT1 *test_var_p = (UDT1 *)memmgr->declare_extern_var(&test_var, "UDT1");
+    ALLOC_INFO* alloc_info = memmgr->get_alloc_info_of(test_var_p);
+    //memmgr->write_JSON_alloc_info(std::cout, alloc_info);
+    memmgr->write_JSON_alloc_info(ss, alloc_info);
+    s = ss.str();
 
-     std::size_t pos;
-     bool found = true;
+    std::size_t pos;
+    bool found = true;
 
-     pos = s.find("name");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("start");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("end");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("num");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("size");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("type");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("stcl");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("TRICK_EXTERN");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("language");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("Language_CPP");
-     if (pos == std::string::npos) found = false;
-     pos = s.find("index");
-     if (pos == std::string::npos) found = false;
-     EXPECT_EQ(found, true);
- }
+    pos = s.find("name");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("start");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("end");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("num");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("size");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("type");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("stcl");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("TRICK_EXTERN");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("language");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("Language_CPP");
+    if (pos == std::string::npos) found = false;
+    pos = s.find("index");
+    if (pos == std::string::npos) found = false;
 
- TEST_F(MM_JSON_intf, Write_Alloc_Info_List ) {
-     std::stringstream ss;
-     std::string s;
-     void *ptr;
-     ptr = memmgr->declare_var("double apple");
-     ptr = memmgr->declare_var("double banana");
-     ptr = memmgr->declare_var("double cherry");
-     ptr = memmgr->declare_var("double date");
-     ptr = memmgr->declare_var("double elderberry");
+    EXPECT_EQ(found, true);
+}
 
-     memmgr->write_JSON_alloc_list(std::cout, 0, 2);
-     memmgr->write_JSON_alloc_list(ss, 0, 2);
-     s = ss.str();
+TEST_F(MM_JSON_intf, Write_Alloc_Info_List ) {
+    std::stringstream ss;
+    std::string s;
+    void *ptr;
+    ptr = memmgr->declare_var("double apple");
+    ptr = memmgr->declare_var("double banana");
+    ptr = memmgr->declare_var("double cherry");
+    ptr = memmgr->declare_var("double date");
+    ptr = memmgr->declare_var("double elderberry");
 
-     std::size_t pos;
-     bool found = true;
+    memmgr->write_JSON_alloc_list(std::cout, 0, 2);
+    memmgr->write_JSON_alloc_list(ss, 0, 2);
+    s = ss.str();
 
-     pos = s.find("alloc_total");
-     if (pos >= std::string::npos) found = false;
-     pos = s.find("chunk_size");
-     if (pos >= std::string::npos) found = false;
-     pos = s.find("chunk_start");
-     if (pos >= std::string::npos) found = false;
-     pos = s.find("alloc_list");
-     if (pos == std::string::npos) found = false;
+    std::size_t pos;
+    bool found = true;
 
-     EXPECT_EQ(found, true);
+    pos = s.find("alloc_total");
+    if (pos >= std::string::npos) found = false;
+    pos = s.find("chunk_size");
+    if (pos >= std::string::npos) found = false;
+    pos = s.find("chunk_start");
+    if (pos >= std::string::npos) found = false;
+    pos = s.find("alloc_list");
+    if (pos == std::string::npos) found = false;
 
- }
+    EXPECT_EQ(found, true);
+
+}
