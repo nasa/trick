743d53394c48cb9f309102421c64aae9aed5a703
Marcus Rockwell <marcusrockwelljr@gmail.com>
2023-06-07 14:11:02 -0500

    Expanded the Data Recording Checkpoint test sim   - Added a case for checkpointing change variables

--- test/SIM_checkpoint_data_recording/models/Foo.hh
+++ test/SIM_checkpoint_data_recording/models/Foo.hh
@@ -3,14 +3,17 @@ class Foo {
     public:
     int a; 
     int b;
+    int q;
 
     void init () {
         a = 0;
         b = 0;
+        q = 0;
     }
 
     void increment () {
         a++;
         b+=2;
+        if(b%a == 0) q++;
     }
 };
