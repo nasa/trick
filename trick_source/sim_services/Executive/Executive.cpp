e83ffc88100565d584dd6b140b299f350a691c6a
nathan.eigbe@axiomspace.com <nathan.eigbe@axiomspace.com>
2024-02-28 12:01:09 -0600

    External link to Standard Messenger

--- trick_source/sim_services/Executive/Executive.cpp
+++ trick_source/sim_services/Executive/Executive.cpp
@@ -12,8 +12,12 @@
 
 Trick::Executive * the_exec ;
 
+
+extern void initializeMessenger();
+
 Trick::Executive::Executive() {
 
+   
     /** @par Detailed Design: */
     the_exec = this ;
 
@@ -141,6 +145,9 @@ Trick::Executive::Executive() {
 
     // Initialize all of default signal handlers
     init_signal_handlers() ;
+
+    /**Initialize the TsHsMessenger*/
+    initializeMessenger();
 }
 
 Trick::Executive::~Executive() {
