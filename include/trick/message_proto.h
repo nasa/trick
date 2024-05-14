a714acda1361c02e54d912cf3e13f3718a1d4ce9
Pherring04 <pherring04@gmail.com>
2024-05-14 12:27:53 -0500

    Revert "Added support for message publishers in C interface"
    This reverts commit e3233891d784f980cbb692ebdca0e978126e535a.

--- include/trick/message_proto.h
+++ include/trick/message_proto.h
@@ -9,9 +9,6 @@
 extern "C" {
 #endif
 
-int message_add_subscriber( void * in_ms ) ;
-int message_remove_subscriber( void * in_ms ) ;
-void * message_get_subscriber( const char * sub_name) ;
 int message_publish(int level, const char *format_msg, ...) ;
 int message_publish_standalone(int level, const char *format_msg, ...) ;
 int send_hs(FILE * fp, const char *format_msg, ...) ;
