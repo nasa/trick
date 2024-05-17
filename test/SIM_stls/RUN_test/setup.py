ac8244421fe89655dce04d76c2f77e0aa769be1b
Marcus Rockwell <marcusrockwell@gmail.com>
2024-05-17 16:56:57 -0500

    Updated SIM_stls test to pass with new checkpoint restrictions

--- test/SIM_stls/RUN_test/setup.py
+++ test/SIM_stls/RUN_test/setup.py
@@ -5,7 +5,7 @@ def main():
     trick.exec_set_job_onoff("the_object.stlc.test", 1, False)
     trick.exec_set_job_onoff("the_object.stlc.print", 1, False)
 
-    trick.add_read( 0.5, 'trick.checkpoint("chkpnt_in")')
+    trick.checkpoint(0.1)
 
     trick.exec_set_freeze_frame(0.10)
     trick.stop(1.0)
