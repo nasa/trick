46f97dba4bb4d4fad2ad42383d29a540fa0c6850
Pherring04 <158035107+Pherring04@users.noreply.github.com>
2024-05-23 10:30:56 -0500

    Water Clock Cleanup (#1711)
    * Cleaned up Water Clock and added C interface support for message subscriptions
    
    * Added support for message publishers in C interface
    
    * Revert "Added support for message publishers in C interface"
    
    This reverts commit e3233891d784f980cbb692ebdca0e978126e535a.

--- trick_sims/SIM_waterclock/models/waterclock/include/waterclock_numeric.h
+++ trick_sims/SIM_waterclock/models/waterclock/include/waterclock_numeric.h
@@ -2,7 +2,7 @@
 PURPOSE: ( Water Clock Numeric Model )
 **************************************************************************/
 
-#ifndef WATRERCLOCK_NUMERIC_H
+#ifndef WATERCLOCK_NUMERIC_H
 #define WATERCLOCK_NUMERIC_H
 
 #include "waterclock.h"
@@ -10,10 +10,10 @@ PURPOSE: ( Water Clock Numeric Model )
 #ifdef __cplusplus
 extern "C" {
 #endif
-int waterclock_integ(WATERCLOCK*);
-int waterclock_deriv(WATERCLOCK*);
-double waterclock_tick_change(WATERCLOCK*);
-double waterclock_overflow_timer(WATERCLOCK*);
+int waterclock_integ(WATERCLOCK* WC);
+int waterclock_deriv(WATERCLOCK* WC);
+double waterclock_tick_change(WATERCLOCK* WC);
+double waterclock_overflow_timer(WATERCLOCK* WC);
 void waterclock_update_water_level(WATERCLOCK* WC);
 #ifdef __cplusplus
 }
