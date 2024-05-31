2d6b68e4a205724ac34c1467d6767946d31251cd
Marcus Rockwell <marcusrockwelljr@gmail.com>
2024-05-31 14:08:40 -0500

    Revert "Edit SIM_stls input file to pass test"
    This reverts commit 2a45c903ce03b153043943049b8051ab6cb98fee.

--- test/SIM_stls/RUN_test/setup.py
+++ test/SIM_stls/RUN_test/setup.py
@@ -1,24 +1,11 @@
 import trick
 
-def setup_checkpoint_event(file_name="chkpnt_in"):
-    chkpnt_event = trick.new_event()
-    chkpnt_event.condition(0, "True")
-    action_index = 0
-    if trick.exec_get_mode() == 5:
-        chkpnt_event.action(action_index, 'trick.freeze()')
-        action_index += 1
-    chkpnt_event.action(action_index, f'trick.checkpoint("{file_name}")')
-    chkpnt_event.activate()
-
-    trick.add_event_after(chkpnt_event, "the_object.stlc.addData")
-
 def main():
     trick.exec_set_job_onoff("the_object.stlc.addData", 1, True)
     trick.exec_set_job_onoff("the_object.stlc.test", 1, False)
     trick.exec_set_job_onoff("the_object.stlc.print", 1, False)
 
-    # trick.add_read( 0.5, 'trick.checkpoint("chkpnt_in")')
-    setup_checkpoint_event()
+    trick.add_read( 0.5, 'trick.checkpoint("chkpnt_in")')
 
     trick.exec_set_freeze_frame(0.10)
     trick.stop(1.0)
