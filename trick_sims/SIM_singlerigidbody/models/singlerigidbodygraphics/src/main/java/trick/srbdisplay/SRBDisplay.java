ad53246904d5a350115a011d390d2cef1ab953d0
AdityaGirish <aditya.girish@nasa.gov>
2024-09-04 10:59:31 -0500

    Fixed triangles and normals.

--- trick_sims/SIM_singlerigidbody/models/singlerigidbodygraphics/src/main/java/trick/srbdisplay/SRBDisplay.java
+++ trick_sims/SIM_singlerigidbody/models/singlerigidbodygraphics/src/main/java/trick/srbdisplay/SRBDisplay.java
@@ -149,14 +149,14 @@ class SingleRigidBodyView extends JPanel { // Main body class of how we view the
 
         veh_triangles = new int[][]
         {
-          {0, 8, 13}, {0, 9, 8}, {0, 14, 9}, // First pentagon using vertices 0, 8, 9, 13, 14
+          {0, 14, 9}, {0, 9, 8}, {0, 8, 13}, // First pentagon using vertices 0, 8, 9, 13, 14
           {13, 15, 0}, {13, 6, 15}, {13, 2, 6}, // Second pentagon using vertices 0, 2, 6, 13, 15
           {2, 13, 8}, {2, 8, 16}, {2, 16, 18}, // Third pentagon using vertices 2, 8, 13, 16, 18
           {18, 6, 2}, {18, 10 ,6}, {18, 4, 10}, // Fourth pentagon using vertices 2, 4, 6, 10, 18
           {4, 18, 16}, {4, 16, 12}, {4, 12, 1}, // Fifth pentagon using 1, 4, 12, 16, 18
           {1, 10, 4}, {1, 11, 10}, {1, 5, 11}, // Sixth pentagon using vertices 1, 4, 5, 10, 11
           {5, 1, 12}, {5, 12, 17}, {5, 17, 19}, // Seventh pentagon using vertices 1, 5, 12, 17, 19
-          {19, 11, 4}, {19, 7, 11}, {19, 3, 7}, // Eight pentagon using vertices 3, 4, 7, 11, 19
+          {19, 11, 5}, {19, 7, 11}, {19, 3, 7}, // Eight pentagon using vertices 3, 4, 7, 11, 19
           {3, 19, 17}, {3, 17, 9}, {3, 9, 14}, // Ninth pentagon using vertices 3, 9, 14, 17, 19
           {14, 7, 3}, {14, 15, 7}, {14, 0, 15}, // Tenth pentagon using vertices 0, 3, 7, 14, 15
           {15, 6, 10}, {15, 10, 11}, {15, 11, 7}, // Top pentagon (perpendicular above to xy plane)
