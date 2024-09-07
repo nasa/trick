6702fd3955ab0e8bebe063eb164dd5d06116c657
Hong Chen <hong.chen-1@nasa.gov>
2024-09-07 14:14:08 -0500

    Added sub second information to message time stamps.

--- trick_source/sim_services/Message/message_publish_standalone.cpp
+++ trick_source/sim_services/Message/message_publish_standalone.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
+#include <sys/time.h>
 
 // message_publish when you don't have a message publisher class
 #define MAX_MSG_HEADER_SIZE 256
@@ -13,18 +14,29 @@ extern "C" int message_publish_standalone(int level, const char * format_msg, ..
     char date_buf[MAX_MSG_HEADER_SIZE];
     char hostname[64];
     time_t date ;
+    struct timeval time_val;
+    // microseconds (converted to seconds) past the last second 
+    double sub_second ;
     va_list args;
 
     va_start(args, format_msg);
     (void) vsnprintf(msg_buf, MAX_MSG_SIZE, format_msg, args);
     va_end(args);
 
-    date = time(NULL) ;
+    gettimeofday(&time_val, NULL);
+    
+    // tv_sec represents seconds since the epoch
+    date = time_val.tv_sec;
+    
+    // tv_usec are microseconds past the last second
+    // Convert microseconds to seconds
+    sub_second = (double)time_val.tv_usec / 1000000;
+
     strftime(date_buf, (size_t) 20, "%Y/%m/%d,%H:%M:%S", localtime(&date));
     (void) gethostname(hostname, (size_t) 48);
-    fprintf(stdout, "|L %d|%s|  |%s|T %d|%.2f| %s" , level,
+    fprintf(stdout, "|L %d|%s.%lf|  |%s|T %d|%.2f| %s" , level,
             // so that we don't call any exec routines, use process id 0 and sim time 0.0
-            date_buf, hostname , 0, 0.0, msg_buf) ;
+            date_buf, sub_second, hostname , 0, 0.0, msg_buf) ;
     fflush(stdout) ;
 
     return (0);
