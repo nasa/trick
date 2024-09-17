428779b59369324d00df6ab5082d367149b32f0a
Pherring04 <pherring04@gmail.com>
2024-09-17 13:33:27 -0500

    Reconciled the heinous offense of using tabs.

--- trick_source/sim_services/ScheduledJobQueue/ScheduledJobQueue.cpp
+++ trick_source/sim_services/ScheduledJobQueue/ScheduledJobQueue.cpp
@@ -92,10 +92,10 @@ int Trick::ScheduledJobQueue::push( JobData * new_job ) {
     /* Increment the size of the queue */
     list_size++ ;
 	
-	int new_job_index = (insert_pt - list) / sizeof(JobData**);
-	if(new_job_index < curr_index) {
-	    curr_index++;	
-	}
+    int new_job_index = (insert_pt - list) / sizeof(JobData**);
+    if(new_job_index < curr_index) {
+        curr_index++;	
+    }
 
     return(0) ;
 
