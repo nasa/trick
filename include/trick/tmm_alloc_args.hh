61f179efeac5f38be40f64be6552990d12ce76ad
Mark Herring <m.ryherr@pm.me>
2024-10-10 06:37:58 -0500

    Documentation updates

--- include/trick/tmm_alloc_args.hh
+++ include/trick/tmm_alloc_args.hh
@@ -13,15 +13,30 @@ LIBRARY DEPENDENCY:
 #include "trick/trick_type_traits.hh"
 
 /**
-
- @tparam T Type to be allocated.
- @tparam Args parameter pack for forward arguments to placement new.
- @details Wrapper around tricks MemoryManager void* declare_var( const char* declaration);)  Users can pass constructor 
- arguments to this function and it will construct type T in the memory return by declare_var
- @param Args  Parameter pack of arguments pased to the constructor
- @return pointer to type T that was just allocated.
+ * @brief Allocates and constructs an object of type `T` using Trick's Memory Manager.
+ * @tparam T Type to be allocated.
+ * @tparam Args parameter pack for forwarding arguments to placement new.
+ * @param args Parameter pack for forwarding arguments to the constructor of type `T`.
+ * 
+ * @details Wrapper around Trick's `void* MemoryManager::declare_var( const char* declaration)`. 
+ * It allocates memory for an object of type `T` and constructs it using placement new.
+ * Users can pass constructor arguments, which will be perfectly forward to `T`'s constructor.
+ * 
+ * @return pointer to type T that was just allocated.
+ * 
+ * @note This uses SFINAE to determine which overload should be selected.
+ * Where `Trick::has_getname<T>::value` evaluates to `false`, this overload is not selected.
+ * When it evalutes to `true`, this overload is used and the return type is `T*`.
+ * 
+ * @example
+ * @code
+ * //Assuming:
+ * //1. TrickTypeToString has a specialization for TestClass.
+ * //2. TestClass has a default constructor.
+ * //3. TestClass has a constructor that takes two arguments.
+ * TestClass* test_class = tmm_alloc_args<TestClass>(arg_1, arg_2);
+ * @endcode
 */
-
 template<typename T, typename ...Args>
 typename std::enable_if<Trick::has_getname<T>::value, T*>::type
 tmm_alloc_args(Args&&... args)
@@ -31,14 +46,34 @@ tmm_alloc_args(Args&&... args)
 }
 
 /**
-
- @tparam T Type to be allocated.
- @tparam Args parameter pack for forward arguments to placement new.
- @details Convenience funcion.  No memory allocation, let's user pass in existing valid memory address and construct
- the type T in that memory via placement new.
- @param Args  Parameter pack of arguments pased to the constructor 
- @param address Memory address the call to T() will be constructed in.
- @return pointer to type T that was just allocated.
+ * @brief Constructs an object of type `T` using placement new.
+ * @tparam T Type to be constructed.
+ * @tparam Args parameter pack for forwarding arguments to placement new.
+ * @param address Address of memory in which T will be constructed.
+ * @param args Parameter pack for forwarding arguments to the constructor of type `T`.
+ * 
+ * @details Generalization of placement new for any type for which a specialization of 
+ * TrickTypeToString exists.
+ * 
+ * @return T Pointer to address.  
+ * 
+ * @note This uses SFINAE to determine which overload should be selected.
+ * Where `Trick::has_getname<T>::value` evaluates to `false`, this overload is not selected.
+ * When it evalutes to `true`, this overload is used and the return type is `T*`.
+ * 
+ * @example
+ * @code
+ * //Assuming:
+ * //1. TrickTypeToString has a specialization for TestClass.
+ * //2. TestClass has a default constructor.
+ * //3. TestClass has a constructor that takes two arguments.
+ * 
+ * TestClass* test_class = trick_MM->declare_var("TestClass");
+ * TestClass* test_class = tmm_alloc_args<TestClass>(test_class, arg_1, arg_2);
+ * // or 
+ * TestClass* test_class = new TestClass();
+ * TestClass* test_class = tmm_alloc_args<TestClass>(test_class, arg_1, arg_2);
+ * @endcode
 */
 template<typename T, typename ...Args>
 typename std::enable_if<Trick::has_getname<T>::value, T*>::type
@@ -48,13 +83,12 @@ tmm_alloc_args(T* address, Args&&... args)
 }
 
 /**
- * 
- @tparam T Type to be allocated.
- @tparam Args parameter pack for forward arguments to placement new.
- @details Version of tmm_alloc_args that will be compiled if TrickTypeToString has no 
- member function 'getName()'.  Static assert will end compilation and print a useful
- error message.
-  @return Technically a nullptr, but this will never compile.
+ * @tparam T Type to be allocated.
+ * @tparam Args parameter pack for forward arguments to placement new.
+ * @details Version of tmm_alloc_args that will be compiled if TrickTypeToString has no 
+ * member function 'getName()'.  Static assert will end compilation and print a useful
+ * error message.
+ * @return Technically a nullptr, but this will never compile.
 */
 template<typename T, typename ...Args>
 typename std::enable_if<!Trick::has_getname<T>::value, T*>::type
--- include/trick/trick_type_traits.hh
+++ include/trick/trick_type_traits.hh
@@ -41,10 +41,31 @@ struct make_void {
 template<typename... Ts>
 using void_t = typename make_void<Ts...>::type;
 
-//Helper struct so we can check if a type has a function "has_getname" defaults to false
+/**
+ * @brief Primary template for `has_getname`.  Defaults to std::false_type.
+ * 
+ * @tparam T The type for which we care if `TrickTypeToString` has a specialization.
+ * 
+ * Default implementation of `has_getname`.
+ * 
+ * @note In the case where `TrickTypeToString` has no member funciton `getName()`,
+ * this template is selected.
+ */
 template<typename T, typename = void_t<>>
 struct has_getname : std::false_type {};
 
+/**
+ * @brief Trait to check if `TrickTypeToString` has a `getName()` member function.
+ * 
+ * @tparam T The type for which we care if `TrickTypeToString` has a specialization.
+ * 
+ * Specialization of `has_getname` for types where `TrickTypeToString<T>` has `getName()` method.
+ * 
+ * @note This uses SFINAE to detect the presence of the method `getName()` in `TrickTypeToString<T>`. If the method
+ * doesn't exist this specialization is discarded, and the primary template is used instead.
+ * 
+ * 
+ */
 template<typename T>
 struct has_getname<T, void_t<decltype(std::declval<TrickTypeToString<T>>().getName())>> : std::true_type {};
 
