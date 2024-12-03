573211e09c1088d0185a0e8a54d1d40fb20afbe6
Pherring04 <pherring04@gmail.com>
2024-12-03 12:49:04 -0600

    Trickify shared on mac fix

--- share/trick/makefiles/trickify.mk
+++ share/trick/makefiles/trickify.mk
@@ -135,16 +135,12 @@ endif
 all: $(TRICKIFY_OBJECT_NAME) $(TRICKIFY_PYTHON_DIR)
 
 $(TRICKIFY_OBJECT_NAME): $(SWIG_OBJECTS) $(IO_OBJECTS) | $(dir $(TRICKIFY_OBJECT_NAME))
-	echo "Enterting trickify.mk"
 	$(info $(call COLOR,Linking)    $@)
 ifeq ($(TRICKIFY_BUILD_TYPE),PLO)
-	echo "Building PLO" 
 	$(call ECHO_AND_LOG,$(LD) $(LD_PARTIAL) -o $@ $(LINK_LISTS))
 else ifeq ($(TRICKIFY_BUILD_TYPE),SHARED)
-	echo "Building SHARED" 
-	$(call ECHO_AND_LOG,$(TRICK_CXX) $(SHARED_OPTIONS) -o $@ $(LINK_LISTS))
+	$(call ECHO_AND_LOG,$(TRICK_CXX) $(SHARED_LIB_OPT) -o $@ $(LINK_LISTS))
 else ifeq ($(TRICKIFY_BUILD_TYPE),STATIC)
-	echo "Building STATIC" 
 	$(call ECHO_AND_LOG,ar rcs $@ $(LINK_LISTS))
 endif
 
