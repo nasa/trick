d0316b5688106add2cf2a459e8c42d9b19befa10
Pherring04 <pherring04@gmail.com>
2024-12-09 14:59:29 -0600

    Moved the py files to their appropriate directory

--- share/trick/pymods/trick/build_trickify_S_source_hh.py
+++ share/trick/pymods/trick/build_trickify_S_source_hh.py
@@ -3,7 +3,7 @@ import os
 path = ""
 if "TRICK_HOME" in os.environ:
     path = os.getenv("TRICK_HOME")
-path += "/share/trick/makefiles/build_trickify.py"
+path += "/share/trick/pymods/trick/build_trickify.py"
 
 exec(open(path).read())
 
