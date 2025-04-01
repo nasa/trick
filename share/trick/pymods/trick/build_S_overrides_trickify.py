9f8f0575c905c07c344754d3750f1a9c57d157f9
plherrin <pherring04@gmail.com>
2025-04-01 13:47:34 -0500

    Addressed issues with trickify s overrides

--- share/trick/pymods/trick/build_S_overrides_trickify.py
+++ share/trick/pymods/trick/build_S_overrides_trickify.py
@@ -4,4 +4,4 @@ import sys
 path = os.path.dirname(os.path.abspath(__file__)) + "/build_trickify.py"
 exec(open(path).read())
 
-build_S_overrides_trickify(sys.argv[1], sys.argv[2], sys.argv[3])
+build_S_overrides_trickify(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
