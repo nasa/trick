83dfb5f41457e976784e13450e77bf5dbde00bc5
plherrin <pherring04@gmail.com>
2025-04-02 10:14:16 -0500

    Fxied some dir paths

--- test/SIM_trickified_shared/S_overrides.mk
+++ test/SIM_trickified_shared/S_overrides.mk
@@ -1,6 +1,6 @@
 LOCAL_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
-include trickified_project/trickified/S_overrides_trickify.mk
+include $(LOCAL_DIR)/trickified_project/trickified/S_overrides_trickify.mk
 
 TRICK_CXXFLAGS += -I$(LOCAL_DIR)/models
 include $(CURDIR)/trickified_project/trickified/myproject.mk
