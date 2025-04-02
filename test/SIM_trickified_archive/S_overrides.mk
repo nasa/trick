7d9c125c58f39223d40a15f2f2787643f9aa4e0c
plherrin <pherring04@gmail.com>
2025-04-02 09:32:04 -0500

    Updated tests to use trickify s overrides

--- test/SIM_trickified_archive/S_overrides.mk
+++ test/SIM_trickified_archive/S_overrides.mk
@@ -1,4 +1,6 @@
 LOCAL_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
+include trickified_project/trickified/S_overrides_trickify.mk
+
 TRICK_CXXFLAGS += -I$(LOCAL_DIR)/models
 include $(CURDIR)/trickified_project/trickified/myproject.mk
