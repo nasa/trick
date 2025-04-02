287f9f3a97f54f9af3b0d753f513e815996d346d
plherrin <pherring04@gmail.com>
2025-04-02 11:00:35 -0500

    CI

--- test/SIM_trickified_archive/S_overrides.mk
+++ test/SIM_trickified_archive/S_overrides.mk
@@ -1,6 +1,11 @@
 LOCAL_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
-include $(LOCAL_DIR)/trickified_project/trickified/S_overrides_trickify.mk
+#Normally would just include S_overrides_trickify.mk like below. Due to the weird order of operations in this test, the file won't be generated before this include statement. I've copied the contents of the file instead.
+#include $(LOCAL_DIR)/trickified_project/trickified/S_overrides_trickify.mk
+TRICK_LDFLAGS += /home/plherrin/trick/test/SIM_trickified_archive/trickified_project/trickified/trickified_myproject.a
+TRICK_EXT_LIB_DIRS += /home/plherrin/trick/test/SIM_trickified_archive/trickified_project/include_bar:/home/plherrin/trick/test/SIM_trickified_archive/trickified_project/include_foo
+TRICK_PYTHON_PATH += /home/plherrin/trick/test/SIM_trickified_archive/trickified_project/trickified/python
+TRICK_SWIG_FLAGS += -I/home/plherrin/trick/test/SIM_trickified_archive/trickified_project/trickified
 
 TRICK_CXXFLAGS += -I$(LOCAL_DIR)/models
 include $(CURDIR)/trickified_project/trickified/myproject.mk
