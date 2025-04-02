82d9231dd3e7c0e3bbf5856a5e9029f6f205c1ac
plherrin <pherring04@gmail.com>
2025-04-02 12:19:26 -0500

    CI

--- test/SIM_trickified_shared/S_overrides.mk
+++ test/SIM_trickified_shared/S_overrides.mk
@@ -2,10 +2,10 @@ LOCAL_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
 #Normally would just include S_overrides_trickify.mk like below. Due to the weird order of operations in this test, the file won't be generated before this include statement. I've copied the contents of the file instead.
 #include $(LOCAL_DIR)/trickified_project/trickified/S_overrides_trickify.mk
-TRICK_LDFLAGS += /home/plherrin/trick/test/SIM_trickified_shared/trickified_project/trickified/trickified_myproject.so
-TRICK_EXT_LIB_DIRS += /home/plherrin/trick/test/SIM_trickified_shared/trickified_project/include_bar:/home/plherrin/trick/test/SIM_trickified_shared/trickified_project/include_foo
-TRICK_PYTHON_PATH += /home/plherrin/trick/test/SIM_trickified_shared/trickified_project/trickified/python
-TRICK_SWIG_FLAGS += -I/home/plherrin/trick/test/SIM_trickified_shared/trickified_project/trickified
+TRICK_LDFLAGS += $(LOCAL_DIR)/trickified_project/trickified/trickified_myproject.so
+TRICK_EXT_LIB_DIRS += $(LOCAL_DIR)/trickified_project/include_bar:$(LOCAL_DIR)/trickified_project/include_foo
+TRICK_PYTHON_PATH += $(LOCAL_DIR)/trickified_project/trickified/python
+TRICK_SWIG_FLAGS += -I$(LOCAL_DIR)/trickified_project/trickified/
 
 TRICK_CXXFLAGS += -I$(LOCAL_DIR)/models
 include $(CURDIR)/trickified_project/trickified/myproject.mk
