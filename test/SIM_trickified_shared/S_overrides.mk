27d728d2421b21ee6fa6bf6544d608d3c90581d0
Hong Chen <hchen99@users.noreply.github.com>
2025-04-09 16:07:07 -0500

    Fixed S_overrides.mk for SIM_trickified_shared to set proper shared lib based on build type.

--- test/SIM_trickified_shared/S_overrides.mk
+++ test/SIM_trickified_shared/S_overrides.mk
@@ -2,7 +2,14 @@ LOCAL_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
 #Normally would just include S_overrides_trickify.mk like below. Due to the weird order of operations in this test, the file won't be generated before this include statement. I've copied the contents of the file instead.
 #include $(LOCAL_DIR)/trickified_project/trickified/S_overrides_trickify.mk
-TRICK_LDFLAGS += $(LOCAL_DIR)/trickified_project/trickified/trickified_myproject.so
+
+BUILD_TYPE := so
+UNAME := $(shell uname)
+ifeq ($(UNAME),Darwin)
+	BUILD_TYPE := dylib
+endif
+
+TRICK_LDFLAGS += $(LOCAL_DIR)/trickified_project/trickified/trickified_myproject.$(BUILD_TYPE)
 TRICK_EXT_LIB_DIRS += $(LOCAL_DIR)/trickified_project/include_bar:$(LOCAL_DIR)/trickified_project/include_foo
 TRICK_PYTHON_PATH += $(LOCAL_DIR)/trickified_project/trickified/python
 TRICK_SWIG_FLAGS += -I$(LOCAL_DIR)/trickified_project/trickified/
