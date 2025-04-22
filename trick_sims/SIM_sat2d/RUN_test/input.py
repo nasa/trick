edc568374eb3340693b948a248c9bc157b1324e3
Thomas Brain <thomas.a.brain@nasa.gov>
2025-04-22 13:14:35 -0500

    cleaned up unit test input files and prevent both realtime and any guis if unit tests are enabled

--- trick_sims/SIM_sat2d/RUN_test/input.py
+++ trick_sims/SIM_sat2d/RUN_test/input.py
@@ -1,4 +1,5 @@
-exec(open("Modified_data/realtime.py").read())
+if not trick_utest.unit_tests.enabled:
+   exec(open("Modified_data/realtime.py").read())
 exec(open("Modified_data/satellite.dr").read())
 
 dyn_integloop.getIntegrator(trick.Runge_Kutta_4, 6)
@@ -6,19 +7,20 @@ dyn_integloop.getIntegrator(trick.Runge_Kutta_4, 6)
 #==========================================
 # Start the Satellite Graphics Client
 #==========================================
-varServerPort = trick.var_server_get_port();
-SatDisplay_path = "models/graphics/dist/SatDisplay.jar"
+if not trick_utest.unit_tests.enabled:
+   varServerPort = trick.var_server_get_port();
+   SatDisplay_path = "models/graphics/dist/SatDisplay.jar"
 
-if (os.path.isfile(SatDisplay_path)) :
-    SatDisplay_cmd = "java -jar " \
-                   + SatDisplay_path \
-                   + " " + str(varServerPort) + " &" ;
-    print(SatDisplay_cmd)
-    os.system( SatDisplay_cmd);
-else :
-    print('==================================================================================')
-    print('SatDisplay needs to be built. Please \"cd\" into ../models/graphics and type \"make\".')
-    print('==================================================================================')
+   if (os.path.isfile(SatDisplay_path)) :
+       SatDisplay_cmd = "java -jar " \
+                      + SatDisplay_path \
+                      + " " + str(varServerPort) + " &" ;
+       print(SatDisplay_cmd)
+       os.system( SatDisplay_cmd);
+   else :
+       print('==================================================================================')
+       print('SatDisplay needs to be built. Please \"cd\" into ../models/graphics and type \"make\".')
+       print('==================================================================================')
 
 trick.stop(5400);
 
--- trick_sims/SIM_sat2d_multidt/RUN_test/unit_test.py
+++ trick_sims/SIM_sat2d_multidt/RUN_test/unit_test.py
@@ -1,5 +1,3 @@
-exec(open("RUN_test/input.py").read())
-
 trick.frame_log_off()
 trick.real_time_disable()
 trick.remove_all_external_applications()
@@ -8,8 +6,10 @@ trick.exec_set_freeze_command(False)
 
 from trick.unit_test import *
 trick_utest.unit_tests.enable()
-trick_utest.unit_tests.set_file_name( os.getenv("TRICK_HOME") + "/trick_test/SIM_sat2d.xml" )
-trick_utest.unit_tests.set_test_name( "IntegTest" )
+trick_utest.unit_tests.set_file_name( os.getenv("TRICK_HOME") + "/trick_test/SIM_sat2d_multidt.xml" )
+trick_utest.unit_tests.set_test_name( "MultiDtIntegTest" )
+
+exec(open("RUN_test/input.py").read())
 
 trick.add_read(5400.0, '''
 TRICK_EXPECT_NEAR(dyn.satellite.pos[0], -1324210.53705666, 0.000001 , "Sat2D" , "FinalXPositionCheck" )
