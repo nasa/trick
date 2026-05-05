760ff3b0809e48bcc53d202a784d5593b00dfe2b
Sean Harmeyer <117398532+sharmeye@users.noreply.github.com>
2026-05-05 15:40:40 -0500

    Add session deletion in VariableServerSessionThread_test
    Fixed memory management by deleting session in the destructor.

--- trick_source/sim_services/VariableServer/test/VariableServerSessionThread_test.cc
+++ trick_source/sim_services/VariableServer/test/VariableServerSessionThread_test.cc
@@ -67,12 +67,13 @@ class VariableServerSessionThread_test : public ::testing::Test {
         MockMessagePublisher message_publisher;
 
 		VariableServerSessionThread_test() { 
-            varserver->set_enabled(1);
+            
+            varserver = new Trick::VariableServer;
+			
+			varserver->set_enabled(1);
             varserver->set_allow_connections(1);
             varserver->set_bypass_ip_check(1);
 
-            // Set up dependencies that haven't been broken
-            varserver = new Trick::VariableServer;
             Trick::VariableServerSessionThread::set_vs_ptr(varserver);
 
             // Set up mocks
@@ -83,6 +84,7 @@ class VariableServerSessionThread_test : public ::testing::Test {
 
 		~VariableServerSessionThread_test() { 
             delete varserver; 
+			delete session;
         }
 
 		void SetUp() {}
