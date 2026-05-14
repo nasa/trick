032b6989d6be0b25f4a521464982336606716dde
Sean Harmeyer <sean.g.harmeyer@nasa.gov>
2026-05-14 17:07:04 -0500

    Interface updates to JPerf

--- trick_source/java/src/main/java/trick/jobperf/TraceViewMenuBar.java
+++ trick_source/java/src/main/java/trick/jobperf/TraceViewMenuBar.java
@@ -1,54 +1,45 @@
 package trick.jobperf;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
 
 /**
-* Class TraceViewMenuBar represents the menu bar of the JobPerf application.
-* It aggregates the following GUI components:
-* JMenuBar [this]
-*     JMenu [fileMenu]
-*         JMenuItem [fileMenuExit], Action: Call System.exit(0);
-*     JMenu [viewMenu]
-*         JMenu [traceSizeMenu]
-*             JMenuItem [traceSizeIncrease], Action: Call traceView.incrementTraceWidth().
-*             JMenuItem [traceSizeDecrease], Action: Call traceView.decrementTraceWidth()
-*/
+ * Class TraceViewMenuBar represents the menu bar of the JobPerf application.
+ *
+ * View menu zoom items:
+ *   Ctrl+Z       — Zoom Out (one level, same as right-click on canvas)
+ *   Ctrl+Shift+Z — Zoom Reset (back to original view)
+ */
 public class TraceViewMenuBar extends JMenuBar implements ActionListener {
-
     private TraceViewCanvas traceView;
+    private TraceViewInputToolBar inputToolBar;
 
-    /**
-     * Constructor
-     * @param tvc the TraceViewCanvas to be controlled by this menu bar.
-     */
-    public TraceViewMenuBar(TraceViewCanvas tvc) {
-        traceView = tvc;
+    public TraceViewMenuBar(TraceViewCanvas tvc, TraceViewInputToolBar tb) {
+        traceView    = tvc;
+        inputToolBar = tb;
 
+        // File menu
         JMenu fileMenu = new JMenu("File");
         JMenuItem fileMenuExit = new JMenuItem("Exit");
         fileMenuExit.setActionCommand("exit");
-        KeyStroke ctrlQ  = KeyStroke.getKeyStroke('Q', InputEvent.CTRL_MASK );
-        fileMenuExit.setAccelerator(ctrlQ);
+        fileMenuExit.setAccelerator(KeyStroke.getKeyStroke('Q', InputEvent.CTRL_MASK));
         fileMenuExit.addActionListener(this);
         fileMenu.add(fileMenuExit);
         add(fileMenu);
 
+        // View menu
         JMenu viewMenu = new JMenu("View");
 
         JMenuItem traceSizeIncrease = new JMenuItem("Increase Trace Width");
         traceSizeIncrease.setActionCommand("increase-trace_width");
-        KeyStroke ctrlPlus  = KeyStroke.getKeyStroke('=', InputEvent.CTRL_MASK );
-        traceSizeIncrease.setAccelerator(ctrlPlus);
+        traceSizeIncrease.setAccelerator(KeyStroke.getKeyStroke('=', InputEvent.CTRL_MASK));
         traceSizeIncrease.addActionListener(this);
         viewMenu.add(traceSizeIncrease);
 
         JMenuItem traceSizeDecrease = new JMenuItem("Decrease Trace Width");
         traceSizeDecrease.setActionCommand("decrease-trace_width");
-        KeyStroke ctrlMinus = KeyStroke.getKeyStroke('-', InputEvent.CTRL_MASK);
-        traceSizeDecrease.setAccelerator(ctrlMinus);
+        traceSizeDecrease.setAccelerator(KeyStroke.getKeyStroke('-', InputEvent.CTRL_MASK));
         traceSizeDecrease.addActionListener(this);
         viewMenu.add(traceSizeDecrease);
 
@@ -56,43 +47,53 @@ public class TraceViewMenuBar extends JMenuBar implements ActionListener {
 
         JMenuItem showFrame = new JMenuItem("Frame Details ...");
         showFrame.setActionCommand("expand-selected-frame");
-        KeyStroke ctrlF = KeyStroke.getKeyStroke('F', InputEvent.CTRL_MASK);
-        showFrame.setAccelerator(ctrlF);
+        showFrame.setAccelerator(KeyStroke.getKeyStroke('F', InputEvent.CTRL_MASK));
         showFrame.addActionListener(this);
         viewMenu.add(showFrame);
 
         JMenuItem showStats = new JMenuItem("Job Statistics ...");
         showStats.setActionCommand("show-job-stats");
-        KeyStroke ctrlV = KeyStroke.getKeyStroke('V', InputEvent.CTRL_MASK);
-        showStats.setAccelerator(ctrlV);
+        showStats.setAccelerator(KeyStroke.getKeyStroke('V', InputEvent.CTRL_MASK));
         showStats.addActionListener(this);
         viewMenu.add(showStats);
 
-        add(viewMenu);
+        viewMenu.addSeparator();
 
+        JMenuItem zoomOut = new JMenuItem("Zoom Out  (one level)");
+        zoomOut.setActionCommand("zoom-out");
+        zoomOut.setAccelerator(KeyStroke.getKeyStroke('Z', InputEvent.CTRL_MASK));
+        zoomOut.addActionListener(this);
+        viewMenu.add(zoomOut);
+
+        JMenuItem zoomReset = new JMenuItem("Zoom Reset  (all levels)");
+        zoomReset.setActionCommand("zoom-reset");
+        zoomReset.setAccelerator(KeyStroke.getKeyStroke('Z', InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
+        zoomReset.addActionListener(this);
+        viewMenu.add(zoomReset);
+
+        add(viewMenu);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        String s = e.getActionCommand();
-        switch (s) {
-            case "increase-trace_width":
-                traceView.incrementTraceWidth();
-            break;
-            case "decrease-trace_width":
-                traceView.decrementTraceWidth();
-            break;
-            case "expand-selected-frame":
-                traceView.displaySelectedFrame();
-            break;
+        switch (e.getActionCommand()) {
+            case "increase-trace_width":  traceView.incrementTraceWidth(); break;
+            case "decrease-trace_width":  traceView.decrementTraceWidth(); break;
+            case "expand-selected-frame": traceView.displaySelectedFrame(); break;
             case "show-job-stats":
                 traceView.jobStats.SortByID();
                 traceView.jobStats.displayJobStatsWindow();
-            break;
-            case "exit":
-                System.exit(0);
-            default:
-                System.out.println("Unknown Action Command:" + s);
-            break;
+                break;
+            case "zoom-out":
+                traceView.zoomOut();
+                inputToolBar.updateZoomButtons();
+                break;
+            case "zoom-reset":
+                traceView.zoomReset();
+                inputToolBar.updateZoomButtons();
+                break;
+            case "exit": System.exit(0); break;
+            default: System.out.println("Unknown Action Command: " + e.getActionCommand());
         }
     }
 }
