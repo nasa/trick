a005f116a52c407e626103bcff54ab6ab08b9bc5
Hong Chen <hchen99@users.noreply.github.com>
2026-06-23 09:24:41 -0500

    ruff check python code and add missing ref log file.

--- test/SIM_stls/RUN_test_dr/input_dr.py
+++ test/SIM_stls/RUN_test_dr/input_dr.py
@@ -1,9 +1,10 @@
 import trick
 
+
 def main():
     trick.exec_set_job_onoff("the_object.stlc.addData", 1, True)
-    trick.exec_set_job_onoff("the_object.stlc.test",    1, False)
-    trick.exec_set_job_onoff("the_object.stlc.print",   1, False)
+    trick.exec_set_job_onoff("the_object.stlc.test", 1, False)
+    trick.exec_set_job_onoff("the_object.stlc.print", 1, False)
 
     # ---------------------------------------------------------------
     # ASCII data recording group for STL primitive containers
@@ -98,6 +99,7 @@ def main():
 
     trick.exec_set_freeze_frame(0.10)
     trick.stop(1.0)
+
 
 if __name__ == "__main__":
     main()
