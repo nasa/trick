0e78e7ae179e8812a45b2effc63845e1604a1b10
brendan fattig <brendan.fattig@nasa.gov>
2026-07-08 16:39:49 -0500

    cmake: transition: move cmake file under cmake/

---
+++ cmake/TestICGLinkedLibs.cpp
@@ -0,0 +1,19 @@
+#include "clang/Basic/Builtins.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Lex/PreprocessorOptions.h"
+#include "clang/Parse/ParseAST.h"
+// `llvm/Support/Host.h` is deprecated in favour of `llvm/TargetParser/Host.h` since clang 17
+#if LIBCLANG_MAJOR > 16
+#include "llvm/TargetParser/Host.h"
+#else
+#include "llvm/Support/Host.h"
+#endif
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
+
+int main() { return 0; }
