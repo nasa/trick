3bab87a4c0ec7662dab4d1260724c93423bd2792
Hong Chen <hchen99@users.noreply.github.com>
2026-07-29 15:48:26 -0500

    Remove the code for writing unsigned char in string literal format in a checkpoint.

--- trick_source/sim_services/CheckPointAgent/ClassicCheckPointAgent.cpp
+++ trick_source/sim_services/CheckPointAgent/ClassicCheckPointAgent.cpp
@@ -764,7 +764,7 @@ std::string Trick::ClassicCheckPointAgent::
             // attr.index[0].size = 0 (not static array)
             // attr.size = sizeof(char)
             // This prevents anonymous allocations from appearing in subsequent checkpoints
-            if ((attr != NULL) && (attr->type == TRICK_CHARACTER || attr->type == TRICK_UNSIGNED_CHARACTER) && ((curr_dim + 1) == attr->num_index)) {
+            if ((attr != NULL) && attr->type == TRICK_CHARACTER && ((curr_dim + 1) == attr->num_index)) {
                 std::stringstream ss;
                 write_quoted_str( ss, (const char*)pointer);
                 reference_string = ss.str();
