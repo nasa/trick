c8d3790430e191b28357380c4b34af4df7539232
brendan fattig <brendan.fattig@nasa.gov>
2026-09-03 14:43:44 -0500

    Remove more old c++ flags

--- trick_source/codegen/Interface_Code_Gen/PrintAttributes.cpp
+++ trick_source/codegen/Interface_Code_Gen/PrintAttributes.cpp
@@ -267,7 +267,7 @@ void PrintAttributes::printEnum(EnumValues* ev) {
         outfile.close() ;
         printSieEnum(&enumValues) ;
     }
-    
+
     if (!isHeaderExcluded(fileName, false)) {
          printer->printEnumMap(enum_map_outfile, ev);
     }
@@ -445,17 +445,11 @@ void PrintAttributes::printIOMakefile() {
     makefile_io_src.open("build/Makefile_io_src") ;
     makefile_io_src
         << "TRICK_IO_CXXFLAGS += -Wno-invalid-offsetof -Wno-old-style-cast -Wno-write-strings -Wno-unused-variable" << std::endl
-        << std::endl
-        << "ifeq ($(IS_CC_CLANG), 0)" << std::endl
-        << "    TRICK_IO_CXXFLAGS += -Wno-unused-local-typedefs -Wno-unused-but-set-variable" << std::endl
-        << "    ifeq ($(shell test $(GCC_MAJOR) -lt 6; echo $$?), 0)" << std::endl
-        << "        TRICK_IO_CXXFLAGS += -std=c++11" << std::endl
-        << "    endif" << std::endl
-        << "endif" << std::endl
-        << "ifeq ($(IS_CC_CLANG), 1)" << std::endl
-        << "    TRICK_IO_CXXFLAGS += -std=c++14" << std::endl
-        << "endif" << std::endl
-        << std::endl
+        << '\n'
+        << "ifeq ($(IS_CC_CLANG), 0)" << '\n'
+        << "    TRICK_IO_CXXFLAGS += -Wno-unused-local-typedefs -Wno-unused-but-set-variable" << '\n'
+        << "endif" << '\n'
+        << '\n'
         << "IO_OBJECTS =" ;
 
     std::map< std::string , std::string >::iterator mit ;
